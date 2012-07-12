a9c5c06db25a6ead5cf0b8c3ab5cbca1ddfaad51
Jason Coleman <jason@strangerstudios.com>
2012-07-12 13:34:57 -0400

    Extra escaping on values passed to the memberslist-csv.php export.

--- adminpages/memberslist-csv.php
+++ adminpages/memberslist-csv.php
@@ -37,10 +37,10 @@
 		
 	if($s)
 	{
-		$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.billing_amount, mu.cycle_period, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0 AND (u.user_login LIKE '%$s%' OR u.user_email LIKE '%$s%' OR um.meta_value LIKE '%$s%') ";
+		$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.billing_amount, mu.cycle_period, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0 AND (u.user_login LIKE '%" . $wpdb->escape($s) . "%' OR u.user_email LIKE '%" . $wpdb->escape($s) . "%' OR um.meta_value LIKE '%" . $wpdb->escape($s) . "%') ";
 	
 		if($l)
-			$sqlQuery .= " AND mu.membership_id = '" . $l . "' ";					
+			$sqlQuery .= " AND mu.membership_id = '" . $wpdb->escape($l) . "' ";					
 			
 		$sqlQuery .= "GROUP BY u.ID ORDER BY user_registered DESC LIMIT $start, $limit";
 	}
@@ -103,11 +103,11 @@
 	header("Content-type: text/csv");
 	//header("Content-type: application/vnd.ms-excel");
 	if($s && $l)
-		header("Content-Disposition: attachment; filename=members_list_" . $l . "_level" . $s . ".csv; size=$size_in_bytes");
+		header("Content-Disposition: attachment; filename=members_list_" . intval($l) . "_level" . sanitize_file_name($s) . ".csv; size=$size_in_bytes");
 	elseif($s)
-		header("Content-Disposition: attachment; filename=members_list_" . $s . ".csv; size=$size_in_bytes");
+		header("Content-Disposition: attachment; filename=members_list_" . sanitize_file_name($s) . ".csv; size=$size_in_bytes");
 	elseif($l)
-		header("Content-Disposition: attachment; filename=members_list_level" . $l . ".csv; size=$size_in_bytes");
+		header("Content-Disposition: attachment; filename=members_list_level" . intval($l) . ".csv; size=$size_in_bytes");
 	else
 		header("Content-Disposition: attachment; filename=members_list.csv; size=$size_in_bytes");
 	
