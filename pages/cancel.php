7d702b4a8bf35f1b27c4b0558eb9ade6072eb670
Jason Coleman <jason@strangerstudios.com>
2012-11-26 19:50:08 -0500

    Button CSS updates. Updating version number.

--- pages/cancel.php
+++ pages/cancel.php
@@ -14,9 +14,9 @@
 <p>Are you sure you want to cancel your membership?</p>
 
 <p>
-	<a class="yeslink" href="<?php echo pmpro_url("cancel", "?confirm=true")?>">Yes, cancel my account</a>
+	<a class="pmpro_yeslink yeslink" href="<?php echo pmpro_url("cancel", "?confirm=true")?>">Yes, cancel my account</a>
 	|
-	<a class="nolink" href="<?php echo pmpro_url("account")?>">No, keep my account</a>
+	<a class="pmpro_nolink nolink" href="<?php echo pmpro_url("account")?>">No, keep my account</a>
 </p>
 <?php } else { ?>
 	<p>Click here to <a href="<?php echo get_home_url()?>">go to the home page</a>.</p>
