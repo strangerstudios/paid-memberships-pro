42d3529029617081e6613c4fb78af8356ea5c1cf
adambware <adamboulware@gmail.com>
2012-12-03 12:38:39 -0500

    Update pages/billing.php
    Card Type should display for Authorize.net gateway (Line 239).
    Line 67 could be changed to show Billing Address info for Authorize.net gateway as well. It appears the class.pmprogateway_authorizenet.php has sufficient function update() to update billing address info for ARB subscription.

--- pages/billing.php
+++ pages/billing.php
@@ -64,7 +64,7 @@
 				}
 			?>                        	                       	                       														          
 										
-			<?php if(empty($pmpro_stripe_lite) && $gateway == "stripe") { ?>
+			<?php if(empty($pmpro_stripe_lite) && $gateway == "stripe") { // Show for $gateway=="authorizenet" ?>
 			<table id="pmpro_billing_address_fields" class="pmpro_checkout" width="100%" cellpadding="0" cellspacing="0" border="0">
 			<thead>
 				<tr>
@@ -236,7 +236,7 @@
 							<?php
 							}
 						?>
-						<?php if(empty($pmpro_stripe_lite) && $gateway == "stripe") { ?>
+						<?php if(empty($pmpro_stripe_lite) && $gateway == "stripe") { // Shouldn't Card Type be displayed for Authorize.net gateway? It is a required field by default. ?>
 						<div>				
 							<label for="CardType">Card Type</label>
 							<select id="CardType" <?php if($gateway != "stripe") { ?>name="CardType"<?php } ?>>
