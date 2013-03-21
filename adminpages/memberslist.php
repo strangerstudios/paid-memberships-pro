63b0c47c7bbb6ed10080285ffbd98d661faa1653
Jason Coleman <jason@strangerstudios.com>
2013-03-21 11:13:58 -0400

    Passing correct vars to members list hooks.

--- adminpages/memberslist.php
+++ adminpages/memberslist.php
@@ -116,7 +116,7 @@
 				<th>First&nbsp;Name</th>
 				<th>Last&nbsp;Name</th>
 				<th>Email</th>
-				<?php do_action("pmpro_memberslist_extra_cols_header", $order_ids);?>
+				<?php do_action("pmpro_memberslist_extra_cols_header", $theusers);?>
 				<th>Billing Address</th>
 				<th>Membership</th>	
 				<th>Fee</th>
@@ -141,7 +141,7 @@
 							<td><?php echo $theuser->first_name?></td>
 							<td><?php echo $theuser->last_name?></td>
 							<td><a href="mailto:<?php echo $theuser->user_email?>"><?php echo $theuser->user_email?></a></td>
-							<?php do_action("pmpro_memberslist_extra_cols_body", $order);?>
+							<?php do_action("pmpro_memberslist_extra_cols_body", $theuser);?>
 							<td>
 								<?php 
 									if(empty($theuser->pmpro_bfirstname))
