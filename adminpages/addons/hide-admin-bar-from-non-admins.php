78ab53bf984f3264b6dd31ddbae03751feb08e74
Jason Coleman <jason@strangerstudios.com>
2013-06-25 16:01:53 +0000

    Fixed activation link for hide admin bar from non admins.

--- adminpages/addons/hide-admin-bar-from-non-admins.php
+++ adminpages/addons/hide-admin-bar-from-non-admins.php
@@ -21,7 +21,7 @@ function pmpro_addon_hide_admin_bar_from_non_admins_widget($addon)
 			<?php if($addon['enabled']) { ?>
 				<a href="<?php echo admin_url("plugins.php");?>" class="button">Enabled</a>
 			<?php } elseif(file_exists(dirname(__FILE__) . "/../../../hide-admin-bar-from-non-admins/hide-admin-bar-from-non-admins.php")) { ?>
-				<a href="<?php echo wp_nonce_url(self_admin_url('plugins.php?action=activate&plugin=hide-admin-bar-from-non-admins/hide-admin-bar-from-non-admins.php'), 'activate-plugin_pmpro-register-helper/hide-admin-bar-from-non-admins.php')?>" class="button button-primary">Activate</a>
+				<a href="<?php echo wp_nonce_url(self_admin_url('plugins.php?action=activate&plugin=hide-admin-bar-from-non-admins/hide-admin-bar-from-non-admins.php'), 'activate-plugin_hide-admin-bar-from-non-admins/hide-admin-bar-from-non-admins.php')?>" class="button button-primary">Activate</a>
 			<?php } else { ?>
 				<a href="<?php echo wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=hide-admin-bar-from-non-admins'), 'install-plugin_hide-admin-bar-from-non-admins'); ?>" class="button button-primary">Download</a>
 			<?php } ?>
