23215fb6752793a6f99fd31339f09705e5329ec5
Jason Coleman <jason@strangerstudios.com>
2013-08-18 17:38:02 +0000

    Removed the pmpro_cron_trial_ending_warnings daily cron so trial ending emails will no longer go out. The function pmpro_cron_trial_ending_warnings() is still there if you want to call it yourself. There were issues on some sites where these emails were going out erroneously and also many ways of doing "custom trials" including setting the subscription start date back a certain number of days was not picked up as a trial by this script anyway.

--- scheduled/crons.php
+++ scheduled/crons.php
@@ -1,37 +1,7 @@
-<?php	
-	add_action("pmpro_cron_expiration_warnings", "pmpro_cron_expiration_warnings");		
-	function pmpro_cron_expiration_warnings()
-	{	
-		global $wpdb;
-		
-		//make sure we only run once a day
-		$today = date("Y-m-d 00:00:00");
-		
-		$pmpro_email_days_before_expiration = apply_filters("pmpro_email_days_before_expiration", 7);
-				
-		//look for memberships that are going to expire within one week (but we haven't emailed them within a week)
-		$sqlQuery = "SELECT mu.user_id, mu.membership_id, mu.startdate, mu.enddate FROM $wpdb->pmpro_memberships_users mu LEFT JOIN $wpdb->usermeta um ON um.user_id = mu.user_id AND um.meta_key = 'pmpro_expiration_notice' WHERE mu.status = 'active' AND mu.enddate IS NOT NULL AND mu.enddate <> '' AND mu.enddate <> '0000-00-00 00:00:00' AND DATE_SUB(mu.enddate, INTERVAL " . $pmpro_email_days_before_expiration . " Day) <= '" . $today . "' AND (um.meta_value IS NULL OR DATE_ADD(um.meta_value, INTERVAL " . $pmpro_email_days_before_expiration . " Day) <= '" . $today . "') ORDER BY mu.enddate";
-
-		$expiring_soon = $wpdb->get_results($sqlQuery);
-				
-		foreach($expiring_soon as $e)
-		{				
-			$send_email = apply_filters("pmpro_send_expiration_warning_email", true, $e->user_id);
-			if($send_email)
-			{
-				//send an email
-				$pmproemail = new PMProEmail();
-				$euser = get_userdata($e->user_id);		
-				$pmproemail->sendMembershipExpiringEmail($euser);
-				
-				printf(__("Membership expiring email sent to %s. ", "pmpro"), $euser->user_email);
-			}
-				
-			//update user meta so we don't email them again
-			update_user_meta($euser->ID, "pmpro_expiration_notice", $today);
-		}
-	}
-	
+<?php			
+	/*
+		Expiring Memberships		
+	*/
 	add_action("pmpro_cron_expire_memberships", "pmpro_cron_expire_memberships");		
 	function pmpro_cron_expire_memberships()
 	{
@@ -63,7 +33,49 @@
 		}
 	}
 	
-	add_action("pmpro_cron_trial_ending_warnings", "pmpro_cron_trial_ending_warnings");	
+	/*
+		Expiration Warning Emails
+	*/
+	add_action("pmpro_cron_expiration_warnings", "pmpro_cron_expiration_warnings");
+	function pmpro_cron_expiration_warnings()
+	{	
+		global $wpdb;
+		
+		//make sure we only run once a day
+		$today = date("Y-m-d 00:00:00");
+		
+		$pmpro_email_days_before_expiration = apply_filters("pmpro_email_days_before_expiration", 7);
+				
+		//look for memberships that are going to expire within one week (but we haven't emailed them within a week)
+		$sqlQuery = "SELECT mu.user_id, mu.membership_id, mu.startdate, mu.enddate FROM $wpdb->pmpro_memberships_users mu LEFT JOIN $wpdb->usermeta um ON um.user_id = mu.user_id AND um.meta_key = 'pmpro_expiration_notice' WHERE mu.status = 'active' AND mu.enddate IS NOT NULL AND mu.enddate <> '' AND mu.enddate <> '0000-00-00 00:00:00' AND DATE_SUB(mu.enddate, INTERVAL " . $pmpro_email_days_before_expiration . " Day) <= '" . $today . "' AND (um.meta_value IS NULL OR DATE_ADD(um.meta_value, INTERVAL " . $pmpro_email_days_before_expiration . " Day) <= '" . $today . "') ORDER BY mu.enddate";
+
+		$expiring_soon = $wpdb->get_results($sqlQuery);
+				
+		foreach($expiring_soon as $e)
+		{				
+			$send_email = apply_filters("pmpro_send_expiration_warning_email", true, $e->user_id);
+			if($send_email)
+			{
+				//send an email
+				$pmproemail = new PMProEmail();
+				$euser = get_userdata($e->user_id);		
+				$pmproemail->sendMembershipExpiringEmail($euser);
+				
+				printf(__("Membership expiring email sent to %s. ", "pmpro"), $euser->user_email);
+			}
+				
+			//update user meta so we don't email them again
+			update_user_meta($euser->ID, "pmpro_expiration_notice", $today);
+		}
+	}
+		
+	/*
+		Trial Ending Emails
+		Commented out as of version 1.7.2 since this caused issues on some sites
+		and doesn't take into account the many "custom trial" solutions that are
+		in the wild (e.g. some trials are actually a delay of the subscription start date)
+	*/	
+	//add_action("pmpro_cron_trial_ending_warnings", "pmpro_cron_trial_ending_warnings");	
 	function pmpro_cron_trial_ending_warnings()
 	{
 		global $wpdb;
@@ -108,3 +120,8 @@
 			update_user_meta($euser->ID, "pmpro_trial_ending_notice", $today);
 		}
 	}
+	
+	
+	
+	
+	
