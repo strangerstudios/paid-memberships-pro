fe57b30ee5b747ec419dfc5e9d4ec305f404ceb9
Jason Coleman <jason@strangerstudios.com>
2013-08-30 13:36:09 +0000

    Fixed loading of local (in the paid-memberships-pro/languages directory) translation files.

--- includes/localization.php
+++ includes/localization.php
@@ -3,12 +3,12 @@ function pmpro_load_textdomain()
 {
     //get the locale
 	$locale = apply_filters("plugin_locale", get_locale(), "pmpro");
-	$mofile = "pmpro-" . $locale . ".mo";
-
+	$mofile = "pmpro-" . $locale . ".mo";	
+	
 	//paths to local (plugin) and global (WP) language files
-	$mofile_local  = dirname(plugin_basename(__FILE__))."/languages/" . $mofile;
+	$mofile_local  = dirname(__FILE__)."/../languages/" . $mofile;
 	$mofile_global = WP_LANG_DIR . '/pmpro/' . $mofile;
-
+	
 	//load global first
     load_textdomain("pmpro", $mofile_global);
 
