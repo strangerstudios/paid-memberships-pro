346a984597d89ff68434652d798744fa7d50e54c
Jason Coleman <jason@strangerstudios.com>
2013-11-12 12:27:29 -0500

    Patch to support versions of TML with a class but no version property, like v6.2.3

--- includes/login.php
+++ includes/login.php
@@ -57,7 +57,7 @@ function pmpro_login_head()
 {
 	$login_redirect = apply_filters("pmpro_login_redirect", true);
 	if((pmpro_is_login_page() || is_page("login") ||
-		class_exists("Theme_My_Login") && version_compare(Theme_My_Login::version, "6.3") >= 0 && (Theme_My_Login::is_tml_page("register") || Theme_My_Login::is_tml_page("login"))
+		class_exists("Theme_My_Login") && property_exists("Theme_My_Login", "version") && version_compare(Theme_My_Login::version, "6.3") >= 0 && (Theme_My_Login::is_tml_page("register") || Theme_My_Login::is_tml_page("login"))
 		)
 		&& $login_redirect
 	)
@@ -65,7 +65,7 @@ function pmpro_login_head()
 		//redirect registration page to levels page
 		if( isset($_REQUEST['action']) && $_REQUEST['action'] == "register" || 
 			isset($_REQUEST['registration']) && $_REQUEST['registration'] == "disabled"	||
-			class_exists("Theme_My_Login") && version_compare(Theme_My_Login::version, "6.3") >= 0 && Theme_My_Login::is_tml_page("register")		
+			class_exists("Theme_My_Login") && property_exists("Theme_My_Login", "version") && version_compare(Theme_My_Login::version, "6.3") >= 0 && Theme_My_Login::is_tml_page("register")		
 		)
 		{
 			//redirect to levels page unless filter is set.
@@ -111,7 +111,7 @@ function pmpro_login_head()
 					exit;
 				}
 			}
-			elseif(class_exists("Theme_My_Login") && version_compare(Theme_My_Login::version, "6.3") >= 0)
+			elseif(class_exists("Theme_My_Login") && property_exists("Theme_My_Login", "version") && version_compare(Theme_My_Login::version, "6.3") >= 0)
 			{
 				//TML > 6.3
 				$link = Theme_My_Login::get_page_link("login");
