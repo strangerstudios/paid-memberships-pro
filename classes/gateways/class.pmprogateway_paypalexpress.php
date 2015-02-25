49400393f5ca1d88fca7711b556ea82ccd196d05
Jason Coleman <jason@strangerstudios.com>
2015-02-25 15:19:52 -0500

    Added PayPal BN code parameters.

--- classes/gateways/class.pmprogateway_paypalexpress.php
+++ classes/gateways/class.pmprogateway_paypalexpress.php
@@ -796,7 +796,7 @@
 			curl_setopt($ch, CURLOPT_POST, 1);
 		
 			// NVPRequest for submitting to server
-			$nvpreq = "METHOD=" . urlencode($methodName_) . "&VERSION=" . urlencode($version) . "&PWD=" . urlencode($API_Password) . "&USER=" . urlencode($API_UserName) . "&SIGNATURE=" . urlencode($API_Signature) . $nvpStr_;
+			$nvpreq = "METHOD=" . urlencode($methodName_) . "&VERSION=" . urlencode($version) . "&PWD=" . urlencode($API_Password) . "&USER=" . urlencode($API_UserName) . "&SIGNATURE=" . urlencode($API_Signature) . "&BUTTONSOURCE=" . urlencode(PaidMembershipsPro_SP) . $nvpStr_;
 					
 			// setting the nvpreq as POST FIELD to curl
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $nvpreq);
