3803f6629a800c712bcf7b5edbc60aab6525d891
Jason Coleman <jason@strangerstudios.com>
2015-07-22 15:32:33 -0400

    Some comments in the cancel preheader.

--- preheaders/cancel.php
+++ preheaders/cancel.php
@@ -4,14 +4,17 @@
 
 	global $current_user, $pmpro_msg, $pmpro_msgt, $pmpro_confirm, $pmpro_error;
 
+	//get level information for current user
 	if($current_user->ID)
 		$current_user->membership_level = pmpro_getMembershipLevelForUser($current_user->ID);
 
+	//if no user or membership level, redirect to levels page
 	if(!isset($current_user->membership_level->ID)) {
 		wp_redirect(pmpro_url("levels"));
 		exit;
-	}	
+	}
 
+	//are we confirming a cancellation?
 	if(isset($_REQUEST['confirm']))
 		$pmpro_confirm = $_REQUEST['confirm'];
 	else
