f19a6b6c9fe5e098c6844bd618ef05f16946e436
Jason Coleman <jason@strangerstudios.com>
2015-09-10 10:41:48 -0400

    Version update.

--- paid-memberships-pro.php
+++ paid-memberships-pro.php
@@ -3,7 +3,7 @@
 Plugin Name: Paid Memberships Pro
 Plugin URI: http://www.paidmembershipspro.com
 Description: Plugin to Handle Memberships
-Version: 1.8.5.4
+Version: 1.8.5.5
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
