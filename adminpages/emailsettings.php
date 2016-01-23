58edd6c431d5cc7dd8d6f78bd928cc9111fd5c3c
Thomas Sjolshagen <thomas@eighty20results.com>
2016-01-23 10:03:57 -0500

    FIX: Make string translatable

--- adminpages/emailsettings.php
+++ adminpages/emailsettings.php
@@ -95,7 +95,7 @@
 				</th>
 				<td>
 					<input type="checkbox" id="only_filter_pmpro_emails" name="only_filter_pmpro_emails" value="1" <?php if(!empty($only_filter_pmpro_emails)) { ?>checked="checked"<?php } ?> />
-					<label for="only_filter_pmpro_emails"><?php _e('If unchecked, all emails from "WordPress &lt;' . $default_from_email . '&gt;" will be filtered to use the above settings.', 'pmpro');?></label>
+					<label for="only_filter_pmpro_emails"><?php printf( __('If unchecked, all emails from "WordPress &lt;%s&gt;" will be filtered to use the above settings.', 'pmpro'),  $default_from_email);?></label>
 				</td>
 			</tr>
 		</tbody>
