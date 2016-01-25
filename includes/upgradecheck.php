f2f822a0d774083f7939626e08e3b554905537a0
Jason Coleman <jason@strangerstudios.com>
2016-01-24 23:19:44 -0500

    Added script to upgradecheck to remove extra pmpro_cron_ cron jobs.

--- includes/upgradecheck.php
+++ includes/upgradecheck.php
@@ -112,24 +112,45 @@ function pmpro_checkForUpgrades()
 		$pmpro_db_version = 1.869;
 	}
 
-	//add level meta table
-	/*
+	//Remove extra cron jobs inserted in version 1.8.7 and 1.8.7.1
 	if($pmpro_db_version < 1.87) {
-
-		pmpro_db_delta();
-		pmpro_upgrade_1_8_7();
-
-		pmpro_setOption("db_version", "1.87");
-		$pmpro_db_version = 1.87;
+		add_action('init', 'pmpro_upgrade_1_8_7');	//needs to run on init
 	}
-	*/
 }
 
+/*
+	Remove extra cron jobs inserted in version 1.8.7 and 1.8.7.1
+*/
 function pmpro_upgrade_1_8_7() {
 	
-	/*
-		Additional upgrade scripts will go here or we will remove it.
-	*/
+	//fix cron jobs
+    $jobs = _get_cron_array();
+
+    // Remove all pmpro cron jobs (for now).
+    foreach( $jobs as $when => $job_array ) {
+
+        foreach($job_array as $name => $job) {
+	        //delete pmpro cron
+	        if ( false !== stripos( $name, 'pmpro_cron') )
+	            unset($jobs[$when][$name]);	     
+    	}
+
+    	//delete empty cron time slots
+    	if( empty($jobs[$when]) )
+	        unset($jobs[$when]);
+    }
+
+    // Save the data
+    _set_cron_array($jobs);
+
+    //add the three we want back
+	wp_schedule_event(current_time('timestamp'), 'daily', 'pmpro_cron_expire_memberships');
+	wp_schedule_event(current_time('timestamp')+1, 'daily', 'pmpro_cron_expiration_warnings');
+	wp_schedule_event(current_time('timestamp'), 'monthly', 'pmpro_cron_credit_card_expiring_warnings');
+
+	pmpro_setOption("db_version", "1.87");
+	$pmpro_db_version = 1.87;
+
 	return 1.87;
 }
 
