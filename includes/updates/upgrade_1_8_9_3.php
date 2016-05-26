048b0631c6f48fdb2ccd65665be62eee4093e869
Jason Coleman <jason@strangerstudios.com>
2016-05-26 17:26:26 +0000

    Start and end date fix is a standard PMPro update now.

--- includes/updates/upgrade_1_8_9_3.php
+++ includes/updates/upgrade_1_8_9_3.php
@@ -1,23 +1,65 @@
 <?php
 /*
-	Fixing users who weren't given an expiration/enddate at checkout.
+	Upgrade to 1.8.9.3
+	Fixing incorrect start and end dates.
 */
-function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
+function pmpro_upgrade_1_8_9_3() {
 	global $wpdb;	
 	
+	//Fixing incorrect start and end dates. (Sets up update via AJAX)
+	$user_ids = $wpdb->get_col("SELECT user_id FROM $wpdb->pmpro_memberships_users WHERE status = 'active' AND modified > '2016-05-19'");
+
+	if(!empty($user_ids))
+		pmpro_addUpdate('pmpro_upgrade_1_8_9_3_ajax');
+
+	pmpro_setOption("db_version", "1.91");
+	return 1.893;
+}
+
+/*
+	We run this part of the update via AJAX from the updates page.
+*/
+function pmpro_upgrade_1_8_9_3_ajax() {
+	global $wpdb;
+	
+	$debug = true;
+	$run = true;
+
 	//some vars
 	$all_levels = pmpro_getAllLevels(true, true);
 
-	if($debug && $run)
-		echo "Running in live mode. The database WILL be updated.\n-----\n";
-	elseif($debug)
-		echo "Running in test mode. The database WILL NOT be updated.\n-----\n";
+	//keeping track of which user we're working on
+	$last_user_id = get_option('pmpro_upgrade_1_8_9_3_last_user_id', 0);
 
 	//get all active users during the period where things may have been broken
-	$user_ids = $wpdb->get_col("SELECT user_id FROM $wpdb->pmpro_memberships_users WHERE status = 'active' AND modified > '2016-05-19'");
+	$user_ids = $wpdb->get_col("SELECT user_id FROM $wpdb->pmpro_memberships_users WHERE status = 'active' AND modified > '2016-05-19' AND user_id > $last_user_id ORDER BY user_id LIMIT 10");
 
-	if(!empty($user_ids)) {
+	//track progress
+	$first_load = get_transient('pmpro_updates_first_load');
+	if($first_load) {
+		$total_users = $wpdb->get_var("SELECT COUNT(user_id) FROM $wpdb->pmpro_memberships_users WHERE status = 'active' AND modified > '2016-05-19' ORDER BY user_id");
+		update_option('pmpro_upgrade_1_8_9_3_total', $total_users, 'no');
+		$progress = 0;
+	} else {
+		$total_users = get_option('pmpro_upgrade_1_8_9_3_total', 0);
+		$progress = get_option('pmpro_upgrade_1_8_9_3_progress', 0);
+	}
+	update_option('pmpro_upgrade_1_8_9_3_progress', $progress + count($user_ids), 'no');
+	global $pmpro_updates_progress;
+	if($total_users > 0)
+		$pmpro_updates_progress = "[" . $progress . "/" . $total_users . "]";
+	else
+		$pmpro_updates_progress = "";
+
+	if(empty($user_ids)) {
+		//done with this update			
+		pmpro_removeUpdate('pmpro_upgrade_1_8_9_3_ajax');
+		delete_option('pmpro_upgrade_1_8_9_3_last_user_id');
+		delete_option('pmpro_upgrade_1_8_9_3_total');
+		delete_option('pmpro_upgrade_1_8_9_3_progress');
+	} else {
 		foreach($user_ids as $user_id) {
+			$last_user_id = $user_id; //keeping track of the last user we processed
 			$user = get_userdata($user_id);
 			
 			//user not found for some reason
@@ -33,7 +75,7 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 			//has a start and end date already
 			if(!empty($user->membership_level->enddate) && !empty($user->membership_level->startdate)) {
 				if($debug)
-					echo "User #" . $user_id . " already has a start and end date.\n";
+					echo "User #" . $user_id . ", " . $user->user_email . " already has a start and end date.\n";
 				continue;
 			}
 
@@ -71,7 +113,8 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 			//get level
 			if(!empty($discount_code_id)) {
 				$sqlQuery    = "SELECT l.id, cl.*, l.name, l.description, l.allow_signups FROM $wpdb->pmpro_discount_codes_levels cl LEFT JOIN $wpdb->pmpro_membership_levels l ON cl.level_id = l.id LEFT JOIN $wpdb->pmpro_discount_codes dc ON dc.id = cl.code_id WHERE dc.code = '" . $discount_code . "' AND cl.level_id = '" . (int) $level_id . "' LIMIT 1";
-		$pmpro_level = $wpdb->get_row( $sqlQuery );
+		
+				$pmpro_level = $wpdb->get_row( $sqlQuery );
 
 				//if the discount code doesn't adjust the level, let's just get the straight level
 				if ( empty( $pmpro_level ) ) {
@@ -91,7 +134,7 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 			//no level for some reason
 			if(empty($pmpro_level) && empty($pmpro_level->id)) {
 				if($debug)
-					echo "No level found with ID #" . $level_id . " for user #" . $user_id . ".\n";
+					echo "No level found with ID #" . $level_id . " for user #" . $user_id . ", " . $user->user_email . ".\n";
 				continue;
 			}
 
@@ -99,7 +142,7 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 			$pmpro_level = apply_filters( "pmpro_checkout_level", $pmpro_level );
 
 			if($debug)
-				echo "Fixing user #" . $user_id . ". "; 
+				echo "User #" . $user_id . ", " . $user->user_email . ". Fixing.\n";
 
 			//calculate and fix start date
 			if(empty($user->membership_level->startdate)) {
@@ -113,7 +156,7 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 					$startdate = $filtered_startdate;
 
 				if($debug)
-					echo "Adding startdate " . $startdate . ". ";
+					echo "- Adding startdate " . $startdate . ".\n";
 				if($run) {
 					$sqlQuery = "UPDATE $wpdb->pmpro_memberships_users SET startdate = '" . esc_sql($startdate) . "' WHERE user_id = $user_id AND membership_id = $level_id AND status = 'active' LIMIT 1";
 					$wpdb->query($sqlQuery);
@@ -134,7 +177,7 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 
 				if(!empty($enddate) && $enddate != "NULL") {
 					if($debug)
-						echo "Adding enddate " . $enddate . ".";
+						echo "- Adding enddate " . $enddate . ".\n";
 					if($run) {
 						$sqlQuery = "UPDATE $wpdb->pmpro_memberships_users SET enddate = '" . esc_sql($enddate) . "' WHERE user_id = $user_id AND membership_id = $level_id AND status = 'active' LIMIT 1";
 						$wpdb->query($sqlQuery);
@@ -155,86 +198,7 @@ function pmpro_upgrade_1_8_9_3($debug = true, $run = false) {
 
 			echo "\n";
 		}
+
+		update_option('pmpro_upgrade_1_8_9_3_last_user_id', $last_user_id, 'no');
 	}
 }
-
-/*
-	Add Menu Item
-*/
-function pmprou_1_8_9_3_add_pages()
-{	
-	//todo don't add the page if the fix is done or skipped
-	add_submenu_page('pmpro-membershiplevels', __('Update Required', 'pmpro'), __('Update Required *', 'pmpro'), 'manage_options', 'pmpro-updaterequired', 'pmprou_1_8_9_3_page');
-}
-add_action('admin_menu', 'pmprou_1_8_9_3_add_pages');
-function pmprou_1_8_9_3_page()
-{
-	//only admins can get this
-	$cap = apply_filters('pmpro_add_member_cap', 'edit_users');
-	if(!function_exists("current_user_can") || (!current_user_can("manage_options") && !current_user_can($cap)))
-	{
-		die(__("You do not have permissions to perform this action.", "pmpro"));
-	}	
-	
-	global $wpdb;
-	require_once(PMPRO_DIR . "/adminpages/admin_header.php");
-
-	$nusers = $wpdb->get_var("SELECT COUNT(user_id) FROM $wpdb->pmpro_memberships_users WHERE status = 'active' AND modified > '2016-05-19'");
-	?>
-	<style>
-		div.pmpro_admin #message {display: none;}
-	</style>
-	<h2><?php _e('PMPro Update - Fixing Start and End Dates For Recent Members', 'pmpro');?></h2>
-
-	<p><?php _e("Paid Memberships Pro version 1.8.9.2, pushed out on May 20th, included a bug that affected all sites running PMPro. Members who checked out under this version were often assigned incorrect start and end dates.", 'pmpro');?></p>
-	<p><?php _e("Clicking the 'Run Fix in Live Mode' button below will find all users who checked out after May 20th and recalculate their start and end dates. For most sites, this script should run quickly and accurately. The script uses the same filters as a normal checkout and should account for addons and custom code you are running that affects start and end dates.", 'pmpro');?>
-	<p><?php _e("If you are using a large number of addons or custom code that affects users' start and end dates, you will definitely want to run the script in test mode to confirm the results first.", 'pmpro');?></p>
-	<p><?php _e(" All users should back up their database before running the fix in live mode.", 'pmpro');?></p>
-
-	<hr />
-
-	<p><?php echo sprintf(__('We detect %s potentially affected user(s) that need to be fixed.', 'pmpro'), $nusers);?></p><hr />
-	
-	<?php
-		if(!empty($_REQUEST['run']) || !empty($_REQUEST['test'])) {
-			?><textarea id="pmpro_updates_status" rows="10" cols="60"><?php
-			
-			if(!empty($_REQUEST['test'])) {
-				//run in test mode
-				pmpro_upgrade_1_8_9_3(true, false);
-			} elseif(!empty($_REQUEST['live'])) {
-				//run in live mode
-				pmpro_upgrade_1_8_9_3(true, true);
-
-				//set option to hide this update
-				update_option('pmpro_update_1_8_9_3', 'fixed', 'no');
-			}
-
-			?></textarea><hr /><?php
-		} elseif(!empty($_REQUEST['skip'])) {
-			update_option('pmpro_update_1_8_9_3', 'skipped', 'no');
-			?><p><?php _e('You have chosen to skip the fix. You may now navigate away from this page.', 'pmpro');?></p><?php
-		}
-	?>
-
-	<?php if(empty($_REQUEST['skip'])) { ?>
-	<ul>
-		<li><a href="<?php echo admin_url('admin.php?page=pmpro-updaterequired&test=1');?>"><?php _e("Run the fix in TEST mode without updating the database.", 'pmpro');?></a></li>
-		<li><a href="<?php echo admin_url('admin.php?page=pmpro-updaterequired&live=1');?>"><?php _e("Run the fix in LIVE mode.", 'pmpro');?></a></li>
-		<li><a href="<?php echo admin_url('admin.php?page=pmpro-updaterequired&skip=1');?>"><?php _e("Skip this update without running the fix.", 'pmpro');?></a></li>
-	</ul>
-	<?php } ?>
-
-	<?php
-	require_once(PMPRO_DIR . "/adminpages/admin_footer.php");
-}
-
-function pmprou_1_8_9_3_admin_init() {
-	//redirect to update if not skipped yet
-	$update = get_option('pmpro_update_1_8_9_3', false);
-	if(empty($update) && !defined('DOING_AJAX') && current_user_can('manage_options') && (empty($_REQUEST['page']) || $_REQUEST['page'] != 'pmpro-updaterequired')) {
-		wp_redirect(admin_url('admin.php?page=pmpro-updaterequired'));
-		exit;
-	}
-}
-add_action('admin_init', 'pmprou_1_8_9_3_admin_init');
