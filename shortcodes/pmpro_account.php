1e97b4dc57a882d0aaad097c1afd935b52ebc049
Jason Coleman <jason@strangerstudios.com>
2016-07-19 12:09:34 -0400

    Don't need to document the bugs inline.

--- shortcodes/pmpro_account.php
+++ shortcodes/pmpro_account.php
@@ -20,11 +20,7 @@ function pmpro_shortcode_account($atts, $content=null, $code="")
 	if(!empty($section))
 		$sections = $section;
 
-	/**
-	* @var $sections - Extract the user-defined sections for the shortcode
-	*
-	* @since 1.9.0 - BUG: Didn't correctly handle whitespace in sections argument.
-	*/
+	//Extract the user-defined sections for the shortcode
 	$sections = array_map('trim',explode(",",$sections));	
 	ob_start();
 	
