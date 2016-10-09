49e2d198677da85af581cb1f353e55abce3d7fe1
Thomas Sjolshagen <thomas@eighty20results.com>
2016-10-09 15:05:53 -0600

    BUG/ENH: Didn't use global variables when loading level information

--- pages/levels.php
+++ pages/levels.php
@@ -1,8 +1,14 @@
 <?php 
 global $wpdb, $pmpro_msg, $pmpro_msgt, $current_user;
+global $pmpro_levels;
+global $pmpro_level_order;
 
-$pmpro_levels = pmpro_getAllLevels(false, true);
-$pmpro_level_order = pmpro_getOption('level_order');
+if (empty( $pmpro_levels ) ) {
+	$pmpro_levels = pmpro_getAllLevels( false, true );
+}
+if ( empty( $pmpro_level_order ) ) {
+	$pmpro_level_order = pmpro_getOption( 'level_order' );
+}
 
 if(!empty($pmpro_level_order))
 {
