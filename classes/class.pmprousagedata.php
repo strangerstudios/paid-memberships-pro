8c5e3b9670d915d2193e0a8d98077472e44a008d
Josh Pollock <Josh@JoshPress.net>
2017-01-31 16:33:15 -0500

    create new class for usage tracking

---
+++ classes/class.pmprousagedata.php
@@ -0,0 +1,226 @@
+<?php
+class PMProUsageData {
+
+	/**
+	 * API URL
+	 *
+	 * @since 1.9
+	 *
+	 * @var string
+	 */
+	protected $apiUrl = 'https://asimov.paidmembershipspro.com';
+
+	/**
+	 * Collected stats
+	 *
+	 * NOTE: is set using $this->collectStats() $this->getStats() is the public getter, which lazy loads it
+	 *
+	 * @since 1.9
+	 *
+	 * @var array
+	 */
+	protected $stats;
+
+	/**
+	 * Name of option key used to track optin status
+	 *
+	 * True means allowed, false means not allowed, null means no answer yet.
+	 *
+	 * @since 1.9
+	 *
+	 * @var string
+	 */
+	protected $optinKey = '_pmpro_tracking_allowed';
+
+	/**
+	 * Name of option key for tracking last time things were sent
+	 *
+	 * Stored as unix timestamp $this->getLastSent() is public getter and can translate to MySQL format
+	 *
+	 * @since 1.9
+	 *
+	 * @var string
+	 */
+	protected $trackingKey = '_pmpro_tracking_last_sent';
+
+	/**
+	 * PMProUsageData constructor.
+	 *
+	 * @since 1.9
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Get usage stats
+	 *
+	 * Will lazy-load data
+	 *
+	 * @since 1.9
+	 *
+	 * @return array
+	 */
+	public function getStats()
+	{
+		if( empty( $this->stats ) ){
+			$this->collectStats();
+		}
+
+		return $this->stats;
+	}
+
+	/**
+	 * Collect the usage data
+	 *
+	 * @TODO this!
+	 *
+	 * @since 1.9
+	 */
+	protected function collectStats()
+	{
+		$this->stats = array();
+	}
+
+	/**
+	 * Record that usage tracking is allowed for this site
+	 *
+	 * @since 1.9
+	 */
+	public function optin()
+	{
+		update_option( $this->optinKey, true );
+	}
+
+	/**
+	 * Record that usage tracking IS NOT allowed for this site
+	 *
+	 * @since 1.9
+	 */
+	public function optOut()
+	{
+		update_option( $this->optinKey, false );
+
+	}
+
+	/**
+	 * Send the data
+	 *
+	 * @since 1.9
+	 *
+	 * @return array|bool|WP_Error Returns false if trying to send more than once in a day. Or will return what wp_remote_response() returns from API call
+	 */
+	public function send()
+	{
+		if( time() < $this->getLastSent() + DAY_IN_SECONDS ){
+			return false;
+		}
+
+		return $this->sendStats();
+
+	}
+
+	/**
+	 * Is tracking allowed?
+	 *
+	 * @since 1.9
+	 *
+	 * @return bool True if allowed. False if not
+	 */
+	public function canTrack()
+	{
+		if( true === $this->getOptinStatus() ){
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Should we ask for optin
+	 *
+	 * @since 1.9
+	 *
+	 * @return bool
+	 */
+	public function shouldAskForOption()
+	{
+		if ( null === $this->getOptinStatus() ){
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Find last time we sent data to API
+	 *
+	 * @since 1.9
+	 *
+	 * @param bool $unix Optional. If true, the default, UNIX timestap is returned. Formatted to human-readable local time if false.
+	 *
+	 * @return string|int
+	 */
+	public function getLastSent( $unix = true )
+	{
+		$time = get_option( $this->trackingKey, 0 );
+		if( ! $unix ){
+			$time = gmdate( 'Y-m-d H:i:s', ( $time + ( get_option( 'gmt_offset' ) * HOUR_IN_SECONDS ) ) );
+		}
+
+		return $time;
+	}
+
+	/**
+	 * Get value of optin tracking key.
+	 *
+	 * @since 1.9
+	 *
+	 * @return bool|null
+	 */
+	protected function getOptinStatus()
+	{
+		return get_option( $this->optinKey, null );
+	}
+
+	/**
+	 * Update last sent tracking
+	 *
+	 * @since 1.9
+	 *
+	 * @param int $time Optional. Time to record, in UNIX timestamp. Defaults to current time.
+	 */
+	protected function writeLastSent( $time = 0 )
+	{
+		if( ! $time ){
+			$time = time();
+		}
+
+		update_option( $this->trackingKey, $time );
+	}
+
+	/**
+	 * Send to remote API
+	 *
+	 * @since 1.9
+	 *
+	 * @return array|WP_Error
+	 */
+	protected function sendStats()
+	{
+		$this->getStats();
+		$r = wp_remote_request( $this->apiUrl,
+			array(
+				'method' => 'PUT',
+				'body' => wp_json_encode( $this->stats )
+			)
+		);
+
+		if( 200 == wp_remote_retrieve_response_code( $r ) ){
+			$this->writeLastSent();
+		}
+
+		return $r;
+
+	}
+}
