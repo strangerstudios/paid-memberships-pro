55d96fe6d2d6412ee1593063e9936ab6a494ff4e
Thomas Sjolshagen <thomas@eighty20results.com>
2017-08-22 19:10:39 -0600

    ENHANCEMENT: Clear old preservable subscription list if the item(s) have timed out BUG FIX: Don't remove membership access for using during stripe.com subscription Updates.

--- services/stripe-webhook.php
+++ services/stripe-webhook.php
@@ -333,42 +333,41 @@
 				/**
 				 * Array of Stripe.com subscription IDs and the timestamp when they were configured as 'preservable'
 				 */
-				$preserve = get_user_meta( $user_id, 'pmpro_stripe_dont_delete', true );
+				$preserve = get_user_meta( $user_id, 'pmpro_stripe_dont_cancel', true );
+				$cancel_membership = true;
 				
 				// Grab the subscription ID from the webhook
-				if ( !empty( $pmpro_stripe_event->data->object->lines->data ) ) {
+				if ( !empty( $pmpro_stripe_event->data->object ) && 'subscription' == $pmpro_stripe_event->data->object->object ) {
 					
-					// Loop through all event lines from the webhook
-					foreach( $pmpro_stripe_event->data->object->lines->data as $item ) {
+					$subscr = $pmpro_stripe_event->data->object;
 					
-						// Check if there's a sub ID to look at (from the webhook)
-						// If it's in the list of preservable subscription IDs, check how long we've been protective.
-						if ( $item->type == 'subscription' && in_array(  $item->id, array_keys( $preserve ) ) ) {
-							
-							$logstr .= "Stripe subscription ({$event_sub_id}) has been flagged for preservation. Will not cancel membership!";
-							$event_sub_id = $item->id;
-						} elseif ( $item->type == 'subscription' && in_array(  $item->id, array_keys( $preserve ) ) && ( intval( $preserve[ $item->id ] ) + ( 3 * DAY_IN_SECONDS ) <= current_time('timestamp' ) ) ) {
-							
-							// Delete the usermeta entry as it's (probably) stale
-							unset( $preserve[$item->id] );
-							update_user_meta( $user_id,'pmpro_stripe_dont_delete', $preserve );
-						}
+					// Check if there's a sub ID to look at (from the webhook)
+					// If it's in the list of preservable subscription IDs, check how long we've been protective.
+					if ( in_array( $subscr->id, array_keys( $preserve ) ) ) {
 						
+						$logstr       .= "Stripe subscription ({$subscr->id}) has been flagged for preservation. Will NOT cancel the membership for {$user_id}!\n";
+						$cancel_membership = false;
+						
+					} else if ( in_array( $subscr->id, array_keys( $preserve ) ) && ( intval( $preserve[ $subscr->id ] ) + ( 3 * DAY_IN_SECONDS ) <= current_time( 'timestamp' ) ) ) {
+						
+						// Delete the usermeta entry as it's (probably) stale
+						unset( $preserve[ $subscr->id ] );
+						update_user_meta( $user_id, 'pmpro_stripe_dont_cancel', $preserve );
 					}
 				}
 				
-				if(!empty($user->ID) && empty( $event_sub_id ) ) {
+				if(!empty($user->ID) && true === $cancel_membership ) {
 					do_action( "pmpro_stripe_subscription_deleted", $user->ID );
 					
 					if ( $old_order->status == "cancelled" ) {
-						$logstr .= "We've already processed this cancellation. Probably originated from WP/PMPro. (Order #" . $old_order->id . ", Subscription Transaction ID #" . $old_order->subscription_transaction_id . ")";
+						$logstr .= "We've already processed this cancellation. Probably originated from WP/PMPro. (Order #{$old_order->id}, Subscription Transaction ID #{$old_order->subscription_transaction_id})\n";
 					} else if ( ! pmpro_hasMembershipLevel( $old_order->membership_id, $user->ID ) ) {
-						$logstr .= "This user has a different level than the one associated with this order. Their membership was probably changed by an admin or through an upgrade/downgrade. (Order #" . $old_order->id . ", Subscription Transaction ID #" . $old_order->subscription_transaction_id . ")";
+						$logstr .= "This user has a different level than the one associated with this order. Their membership was probably changed by an admin or through an upgrade/downgrade. (Order #{$old_order->id}, Subscription Transaction ID #{$old_order->subscription_transaction_id})\n";
 					} else {
 						//if the initial payment failed, cancel with status error instead of cancelled					
 						pmpro_cancelMembershipLevel( $old_order->membership_id, $old_order->user_id, 'cancelled' );
 						
-						$logstr .= "Cancelled membership for user with id = " . $old_order->user_id . ". Subscription transaction id = " . $old_order->subscription_transaction_id . ".";
+						$logstr .= "Cancelled membership for user with id = {$old_order->user_id}. Subscription transaction id = {$old_order->subscription_transaction_id}.\n";
 						
 						//send an email to the member
 						$myemail = new PMProEmail();
@@ -377,6 +376,12 @@
 						//send an email to the admin
 						$myemail = new PMProEmail();
 						$myemail->sendCancelAdminEmail( $user, $old_order->membership_id );
+					}
+					
+					// Try to delete the usermeta entry as it's (probably) stale
+					if ( isset( $preserve[$old_order->subscription_transaction_id])) {
+						unset( $preserve[$old_order->subscription_transaction_id]);
+						update_user_meta( $user_id, 'pmpro_stripe_dont_cancel', $preserve );
 					}
 					
 					$logstr .= "Subscription deleted for user ID #" . $user->ID . ". Event ID #" . $pmpro_stripe_event->id . ".";
@@ -393,6 +398,7 @@
 					$logstr .= "Could also be a subscription managed by a different app or plugin. Event ID # {$pmpro_stripe_event->id}.";
 					pmpro_stripeWebhookExit();
 				}
+				
 			} else {
 				$logstr .= "Stripe tells us a subscription is deleted, but we could not find the order for that subscription. Could be a subscription managed by a different app or plugin. Event ID #" . $pmpro_stripe_event->id . ".";
 				pmpro_stripeWebhookExit();
