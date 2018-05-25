3cc05ea739148e64cdfc2cd071fc25b77d05e4d0
David Parker <dparker837@verizon.net>
2018-05-25 09:34:00 -0400

    fixed formatting issue

--- blocks/init.php
+++ blocks/init.php
@@ -20,11 +20,11 @@ require_once PMPRO_DIR . '/blocks/checkout-page/index.php';
 require_once PMPRO_DIR . '/blocks/confirmation-page/index.php';
 require_once PMPRO_DIR . '/blocks/invoice-page/index.php';
 require_once PMPRO_DIR . '/blocks/levels-page/index.php';
-//require_once PMPRO_DIR . '/blocks/member/index.php';
-//require_once PMPRO_DIR . '/blocks/membership/index.php';
+// require_once PMPRO_DIR . '/blocks/member/index.php';
+// require_once PMPRO_DIR . '/blocks/membership/index.php';
 
 /**
- * Enqueue block editor only JavaScript
+ * Enqueue block editor only JavaScript and CSS
  */
 function pmpro_block_editor_scripts() {
 
@@ -39,6 +39,15 @@ function pmpro_block_editor_scripts() {
 		filemtime( plugin_dir_path( __FILE__ ) . $block_path )
 	);
 
+	/*
+	// Enqueue optional editor only styles.
+	wp_enqueue_style(
+		'pmpro-editor-css',
+		plugins_url( $editor_style_path, __FILE__ ),
+		[ 'wp-blocks' ],
+		filemtime( plugin_dir_path( __FILE__ ) . $editor_style_path )
+	);
+	*/
 }
 
 // Hook scripts function into block editor hook.
@@ -46,10 +55,11 @@ add_action( 'enqueue_block_editor_assets', 'pmpro_block_editor_scripts' );
 
 
 /**
- * Enqueue front end and editor JavaScript
+ * Enqueue front end and editor JavaScript and CSS
  */
 function pmpro_block_scripts() {
 	$block_path = '/../js/frontend.blocks.js';
+	$style_path = '/../css/blocks.style.css';
 
 	// Enqueue the bundled block JS file.
 	wp_enqueue_script(
@@ -57,6 +67,14 @@ function pmpro_block_scripts() {
 		plugins_url( $block_path, __FILE__ ),
 		[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-api' ],
 		filemtime( plugin_dir_path( __FILE__ ) . $block_path )
+	);
+
+	// Enqueue frontend and editor block styles.
+	wp_enqueue_style(
+		'pmpro-blocks-css',
+		plugins_url( $style_path, __FILE__ ),
+		[ 'wp-blocks' ],
+		filemtime( plugin_dir_path( __FILE__ ) . $style_path )
 	);
 
 }
