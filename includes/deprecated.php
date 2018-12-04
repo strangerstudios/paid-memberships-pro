fe815a1d84013661f27752d66376fbecc561f8ba
Jason Coleman <jason@strangerstudios.com>
2018-12-04 16:00:46 -0500

    This needed to be a lot more different for filters vs hooks.

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -6,39 +6,27 @@
  */
 
 /**
- * Handle renamed hooks
+ * Check for deprecated filters.
  */
-global $pmpro_map_deprecated_hooks;
-
-$pmpro_map_deprecated_hooks = array(
-	'pmpro_getfile_extension_blocklist'    => 'pmpro_getfile_extension_blacklist',
-);
-
-// anonymous function used below is only supported in php 5.3+
-foreach ( $pmpro_map_deprecated_hooks as $new => $old ) {
-	// assumes hooks with no parameters
-	if ( version_compare( phpversion(), '5.3.0', '>=' ) ) {
-		// Using anonmyous functions for PHP 5.3+
-		$func = function() use ( $new, $old ) {
-			pmpro_maybe_show_deprecated_hook_message( $new, $old );
-		};
-	} else {
-		// Using create_function for PHP 5.2
-		$func = create_function( '', "pmpro_maybe_show_deprecated_hook_message( '$new', '$old' );" );
-	}
-	add_action( $new, $func );
-}
-
-function pmpro_maybe_show_deprecated_hook_message( $new, $old ) {
+function pmpro_init_check_for_deprecated_filters() {
 	global $wp_filter;
-	if ( has_filter( $old ) ) {
-		/* translators: 1: the old hook name, 2: the new or replacement hook name */
-		trigger_error( sprintf( esc_html__( 'The %1$s hook has been deprecated in Paid Memberships Pro. Please use the %2$s hook instead.', 'paid-memberships-pro' ), $old, $new ) );
-		
-		foreach( $wp_filter[$old]->callbacks as $priority => $callbacks ) {
-			foreach( $callbacks as $callback ) {
-				add_filter( $new, $callback['function'], $priority, $callback['accepted_args'] ); 
+	
+	$pmpro_map_deprecated_filters = array(
+		'pmpro_getfile_extension_blocklist'    => 'pmpro_getfile_extension_blacklist',
+	);
+	
+	foreach ( $pmpro_map_deprecated_filters as $new => $old ) {
+		if ( has_filter( $old ) ) {
+			/* translators: 1: the old hook name, 2: the new or replacement hook name */
+			trigger_error( sprintf( esc_html__( 'The %1$s hook has been deprecated in Paid Memberships Pro. Please use the %2$s hook instead.', 'paid-memberships-pro' ), $old, $new ) );
+			
+			// Add filters back using the new tag.
+			foreach( $wp_filter[$old]->callbacks as $priority => $callbacks ) {
+				foreach( $callbacks as $callback ) {
+					add_filter( $new, $callback['function'], $priority, $callback['accepted_args'] ); 
+				}
 			}
 		}
 	}
 }
+add_action( 'init', 'pmpro_init_check_for_deprecated_filters', 99 );
