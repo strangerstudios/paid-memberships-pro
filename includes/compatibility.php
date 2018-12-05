7f0c2b7ea028e8eefab8756b9de89bec71f8fed5
ideadude <jason@strangerstudios.com>
2018-12-05 15:29:23 -0500

    Some edits to compat stuff.

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -1,33 +1,33 @@
 <?php
 
-/* 
- * Function to check if certain page builders are installed and activated and if found dynamically load the relevant compatibility files.
- * Compatability files to load "the_content" filter later.
+/**
+ * Check if certain plugins or themes are installed and activated
+ * and if found dynamically load the relevant /includes/compatibility/ files.
  */
-
 function pmpro_compatibility_checker () {
-
     $compat_checks = array(
-    array(
-        'file' => 'siteorigin.php',
-        'check_type' => 'constant',
-        'check_value' => 'SITEORIGIN_PANELS_VERSION',
-    ),
-    array(
-        'file' => 'elementor.php',
-        'check_type' => 'constant', 
-        'check_value' => 'ELEMENTOR_VERSION'
-    ),
-    array(
-        'file' => 'beaver-builder.php',
-        'check_type' => 'constant', 
-        'check_value' => 'FL_BUILDER_VERSION'
-    ),
-);
+        array(
+            'file' => 'siteorigin.php',
+            'check_type' => 'constant',
+            'check_value' => 'SITEORIGIN_PANELS_VERSION',
+        ),
+        array(
+            'file' => 'elementor.php',
+            'check_type' => 'constant', 
+            'check_value' => 'ELEMENTOR_VERSION'
+        ),
+        array(
+            'file' => 'beaver-builder.php',
+            'check_type' => 'constant', 
+            'check_value' => 'FL_BUILDER_VERSION'
+        ),
+    );
 
     foreach ( $compat_checks as $key => $value ) {
-        if ( $value['check_type'] == 'constant' && defined( $value['check_value'] ) ) {
-                include( PMPRO_DIR . '/includes/compatibility/' . $value['file'] ) ;
+        if ( ( $value['check_type'] == 'constant' && defined( $value['check_value'] ) )
+          || ( $value['check_type'] == 'function' && function_exists( $value['check_value'] ) )
+          || ( $value['check_type'] == 'class' && class_exists( $value['check_value'] ) ) ) {
+            include( PMPRO_DIR . '/includes/compatibility/' . $value['file'] ) ;
         }
     }
 }
--- includes/compatibility/beaver-builder.php
+++ includes/compatibility/beaver-builder.php
@@ -1,16 +1,10 @@
 <?php 
-
-/* 
- * Load "the_content" filter later when Beaver Builder is installed and activated.
+/** 
+ * Beaver Builder Compatibility
  */
-
-
-
- function pmpro_beaver_builder_compatibility() {
-  	if ( defined('PMPRO_VERSION') ) {
- 		// Filter members-only content later so that the builder's filters run before PMPro.
- 		remove_filter('the_content', 'pmpro_membership_content_filter', 5);
- 		add_filter('the_content', 'pmpro_membership_content_filter', 15);
- 	}
- }
- add_action( 'init', 'pmpro_beaver_builder_compatibility' );
+function pmpro_beaver_builder_compatibility() {
+	// Filter members-only content later so that the builder's filters run before PMPro.
+	remove_filter('the_content', 'pmpro_membership_content_filter', 5);
+	add_filter('the_content', 'pmpro_membership_content_filter', 15);
+}
+add_action( 'init', 'pmpro_beaver_builder_compatibility' );
