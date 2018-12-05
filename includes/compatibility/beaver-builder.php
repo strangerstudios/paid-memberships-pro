fd4c7a964b26ce230f76d481e944e5075aef0203
Travis Lima <hello@travislima.com>
2018-12-05 14:31:17 +0200

    Update beaver-builder.php

--- includes/compatibility/beaver-builder.php
+++ includes/compatibility/beaver-builder.php
@@ -6,11 +6,11 @@
 
 
 
- function beaver_builder_compatibility_for_pmpro() {
+ function pmpro_beaver_builder_compatibility() {
   	if ( defined('PMPRO_VERSION') ) {
  		// Filter members-only content later so that the builder's filters run before PMPro.
  		remove_filter('the_content', 'pmpro_membership_content_filter', 5);
  		add_filter('the_content', 'pmpro_membership_content_filter', 15);
  	}
  }
- add_action( 'init', 'beaver_builder_compatibility_for_pmpro' );
+ add_action( 'init', 'pmpro_beaver_builder_compatibility' );
