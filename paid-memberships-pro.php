a99693ea6c6f7e0a2649f5faff0409d6da4a1054
ideadude <jason@strangerstudios.com>
2018-12-26 14:25:05 -0500

    require php 5.3.29 for blocks

--- paid-memberships-pro.php
+++ paid-memberships-pro.php
@@ -49,7 +49,11 @@ require_once( PMPRO_DIR . '/includes/filters.php' );                // filters, 
 require_once( PMPRO_DIR . '/includes/reports.php' );                // load reports for admin (reports may also include tracking code, etc)
 require_once( PMPRO_DIR . '/includes/admin.php' );					// admin notices and functionality
 require_once( PMPRO_DIR . '/includes/adminpages.php' );             // dashboard pages
-require_once( PMPRO_DIR . '/blocks/blocks.php' );             		// Gutenberg blocks
+
+if ( version_compare( PHP_VERSION, '5.3.29', '>=' ) ) {
+	require_once( PMPRO_DIR . '/blocks/blocks.php' );             	// Gutenberg blocks
+}
+
 require_once( PMPRO_DIR . '/includes/services.php' );               // services loaded by AJAX and via webhook, etc
 require_once( PMPRO_DIR . '/includes/metaboxes.php' );              // metaboxes for dashboard
 require_once( PMPRO_DIR . '/includes/profile.php' );                // edit user/profile fields
