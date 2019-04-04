de31533f574bd18fdaf49ede97705322f1eb774f
Andrew Lima <andrew@arctek.co.za>
2019-04-04 14:58:27 +0200

    removed 'membership_code' from attribute.
    Bug Fix: Removed the "membership_code" attribute from the `[pmpro_member]` as it was non existent in our getmembershiplevelforuser call.
    
    ENHANCEMENT: added membership_description and membership_confirmation attributes in the shortcode. This may be useful for users.

--- shortcodes/pmpro_member.php
+++ shortcodes/pmpro_member.php
@@ -24,8 +24,9 @@ function pmpro_member_shortcode($atts, $content=null, $code='')
 	$pmpro_level_fields = array(
 		'membership_id',
 		'membership_name',
+		'membership_description',
+		'membership_confirmation',
 		'membership_code_id',
-		'membership_code',
 		'membership_initial_payment',
 		'membership_billing_amount',
 		'membership_cycle_number',
