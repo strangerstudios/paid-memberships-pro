38aabe646fad24e7b672c5a927b8865cd9cbc704
Andrew Lima <andrew@arctek.co.za>
2019-06-21 15:30:55 +0200

    Only show message if free levels are available.
    Only show this message if free levels are available.

--- adminpages/paymentsettings.php
+++ adminpages/paymentsettings.php
@@ -134,7 +134,9 @@
 							}
 						?>
 					</select>
-					<div id='pmpro-default-gateway-message' style="display:none;"><small><?php echo __( 'This gateway is for membership sites with Free levels or for sites that accept payment offline.', 'paid-memberships-pro' ) . '<br/>' . __( 'It is not connected to a live gateway environment and cannot accept payments.', 'paid-memberships-pro' ); ?></small></div>
+					<?php if( pmpro_onlyFreeLevels() ) { ?>
+						<div id='pmpro-default-gateway-message' style="display:none;"><small><?php echo __( 'This gateway is for membership sites with Free levels or for sites that accept payment offline.', 'paid-memberships-pro' ) . '<br/>' . __( 'It is not connected to a live gateway environment and cannot accept payments.', 'paid-memberships-pro' ); ?></small></div>
+					<?php } ?>
 				</td>
 			</tr>
 			<tr>
