ae262a1687564aff689a69edae5a751beafc3193
ideadude <jason@strangerstudios.com>
2019-06-22 09:40:49 -0400

    Adding unit test stuff

---
+++ tests/tests-has-membership-level.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Test the pmpro_hasMembershipLevel() function.
+ */
+class Test_hasMembershipLevel extends WP_UnitTestCase {
+	/**
+	 * Give a user a level and test that they have it.
+	 */
+	function test_pmpro_give_user_level() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+        
+        // Create a user
+        
+        // Create a level
+        
+        // Give the user a level
+        
+        // Assert that the user has the level
+	}
+}
