fb0459df0fae32130eb176e54db21852cc0c9664
ideadude <jason@strangerstudios.com>
2019-06-22 10:25:30 -0400

    Bootrap activates PMPro now. Changing a bit of config.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,14 +12,21 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
 	exit( 1 );
 }
+
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 /**
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/paid-memberships-pro.php';
+	require dirname( __FILE__ ) . '/../paid-memberships-pro.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+// Activate PMPro and run upgrade check to setup DB/etc
+echo "Installing Paid Memberships Pro...\n";
+activate_plugin( 'paid-memberships-pro/paid-memberships-pro.php' );
+pmpro_checkForUpgrades();
