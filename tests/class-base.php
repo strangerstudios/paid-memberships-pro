0bb9bec87d7ec7cf2d77d5ae732f832e2b27e068
Mike Auteri <mauteri@gmail.com>
2019-07-03 10:02:09 -0400

    Clear PMP data from database after tests run.

--- tests/class-base.php
+++ tests/class-base.php
@@ -24,6 +24,35 @@ abstract class Base Extends \WP_UnitTestCase {
 		return $factory;
 	}
 
+	/**
+	 * Runs the routine after all tests have been run.
+	 */
+	public static function tearDownAfterClass() {
+		self::_delete_all_pmp_data();
+
+		parent::tearDownAfterClass();
+	}
+
+	private static function _delete_all_pmp_data() {
+		global $wpdb;
+
+		$tables = [
+			$wpdb->pmpro_discount_codes,
+			$wpdb->pmpro_discount_codes_levels,
+			$wpdb->pmpro_discount_codes_uses,
+			$wpdb->pmpro_membership_levelmeta,
+			$wpdb->pmpro_membership_levels,
+			$wpdb->pmpro_membership_orders,
+			$wpdb->pmpro_memberships_categories,
+			$wpdb->pmpro_memberships_pages,
+			$wpdb->pmpro_memberships_users,
+		];
+
+		foreach ( $tables as $table ) {
+			$wpdb->query( "DELETE FROM {$table}" );
+		}
+	}
+
 } // end of class
 
 //EOF
