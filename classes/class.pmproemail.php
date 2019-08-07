f7fb9294e8ca8a553c3845e73b70937d0f308c8b
fairlight-excalibur <lidgrt@gmail.com>
2019-08-07 12:20:31 -0500

    added sendPaymentActionRequiredEmail, sendPaymentActionRequiredAdminEmail, and relevant templates

--- classes/class.pmproemail.php
+++ classes/class.pmproemail.php
@@ -953,6 +953,82 @@
 
 			return $this->sendEmail();
 		}
+
+		function sendPaymentActionRequiredEmail($user = NULL, $order = NULL, $invoice_url = NULL)
+		{
+			global $wpdb, $current_user;
+			if(!$user)
+				$user = $current_user;
+			
+			if(!$user || !$order)
+				return false;
+
+			// if an invoice URL wasn't passed in, grab it from the order
+			if(empty($invoice_url) && isset($order->invoice_url))
+				$invoice_url = $order->invoice_url;
+
+			// still no invoice URL? bail
+			if(empty($invoice_url))
+				return false;
+				
+			$this->email = $user->user_email;
+			$this->subject = sprintf(__("Payment action required for your %s membership", 'paid-memberships-pro' ), get_option("blogname"));	
+			
+			$this->template = "payment_action";
+
+			$this->template = apply_filters("pmpro_email_template", $this->template, $this);
+
+			$this->data = array(
+								"subject" => $this->subject, 
+								"name" => $user->display_name, 
+								"display_name" => $user->display_name,
+								"user_login" => $user->user_login,
+								"sitename" => get_option("blogname"),
+								"siteemail" => pmpro_getOption("from_email"),
+								"invoice_url" => $invoice_url,
+							);
+						
+			return $this->sendEmail();
+		}
+
+		function sendPaymentActionRequiredAdminEmail($user = NULL, $order = NULL, $invoice_url = NULL)
+		{
+			global $wpdb, $current_user;
+			if(!$user)
+				$user = $current_user;
+			
+			if(!$user || !$order)
+				return false;
+
+			// if an invoice URL wasn't passed in, grab it from the order
+			if(empty($invoice_url) && isset($order->invoice_url))
+				$invoice_url = $order->invoice_url;
+
+			// still no invoice URL? bail
+			if(empty($invoice_url))
+				return false;
+				
+			$this->email = get_bloginfo("admin_email");
+			$this->subject = sprintf(__("Payment action required: membership for %s at %s", 'paid-memberships-pro' ), $user->user_login, get_option("blogname"));	
+			
+			$this->template = "payment_action_admin";
+
+			$this->template = apply_filters("pmpro_email_template", $this->template, $this);
+
+			$this->data = array(
+								"subject" => $this->subject, 
+								"name" => $user->display_name, 
+								"display_name" => $user->display_name,
+								"user_login" => $user->user_login,
+								"sitename" => get_option("blogname"),
+								"siteemail" => pmpro_getOption("from_email"),
+								"user_email" => $user->user_email,
+								"invoice_url" => $invoice_url,
+							);
+						
+			return $this->sendEmail();
+		}
+
 		
 		/**
 		 * Load the text for each default email template.
