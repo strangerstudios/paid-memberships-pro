753b9e6020b979c8c262cc1578663f597cee2fc4
Jason Coleman <jason@strangerstudios.com>
2019-08-08 19:50:03 -0400

    prefixing pmpro_askfirst function

--- js/paid-memberships-pro.js
+++ js/paid-memberships-pro.js
@@ -4,10 +4,20 @@
  * @param	text	The prompt, i.e. are you sure?
  * @param	url		The url to redirect to.
  */
-function askfirst( text, url ) {
+function pmpro_askfirst( text, url ) {
 	var answer = window.confirm( text );
 
 	if ( answer ) {
 		window.location = url;
 	}
+}
+
+/**
+ * Deprecated in v2.1
+ * In case add-ons/etc are expecting the non-prefixed version.
+ */
+if ( typeof askfirst !== 'function' ) {
+    function askfirst( text, url ) {
+        return pmpro_askfirst( text, url );
+    }
 }
