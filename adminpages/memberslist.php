f51cf2b3f0743cb89381be6130a6107f4b368c0a
dparker1005 <dlparker1005@gmail.com>
2019-11-19 11:13:05 -0500

    Fixing memberslist csv export options

--- adminpages/memberslist.php
+++ adminpages/memberslist.php
@@ -5,10 +5,20 @@ global $user_list_table;
 $user_list_table = new PMPro_Members_List_Table();
 $user_list_table->prepare_items();
 require_once dirname( __DIR__ ) . '/adminpages/admin_header.php';
+
+// Build CSV export link.
+$csv_export_link = admin_url( 'admin-ajax.php' ) . '?action=memberslist_csv';
+if ( isset( $_REQUEST['s'] ) ) {
+	$csv_export_link .= '&s=' . esc_attr( sanitize_text_field( trim( $_REQUEST['s'] ) ) );
+}
+if ( isset( $_REQUEST['l'] ) ) {
+	$csv_export_link .= '&l=' . sanitize_text_field( trim( $_REQUEST['l'] ) );
+}
+
 // Render the List Table.
 ?>
 	<h2><?php _e( 'PMPro Members List Table', 'paid-memberships-pro' ); ?>
-	<a target="_blank" href="<?php echo admin_url( 'admin-ajax.php' ); ?>?action=memberslist_csv" class="add-new-h2"><?php _e( 'Export to CSV', 'paid-memberships-pro' ); ?></a>
+	<a target="_blank" href="<?php echo esc_url( $csv_export_link ); ?>" class="add-new-h2"><?php _e( 'Export to CSV', 'paid-memberships-pro' ); ?></a>
 	</h2>
 	<?php do_action( 'pmpro_memberslist_before_table' ); ?>
 		<div id="member-list-table">			
