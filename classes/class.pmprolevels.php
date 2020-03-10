74813b701cb936e23c4590d7215b2e307c5bba2b
Andrew Lima <andrew@arctek.co.za>
2020-03-10 15:04:06 +0200

    Include categories
    Include categories for level object.

--- classes/class.pmprolevels.php
+++ classes/class.pmprolevels.php
@@ -27,6 +27,7 @@ class PMPro_Membership_Level{
         $this->allow_signups = '';
         $this->expiration_number = '';
         $this->expiration_period = '';
+        $this->categories = array();
 
         return $this;
 
@@ -39,6 +40,7 @@ class PMPro_Membership_Level{
     function get_membership_level( $id ) {
 
         $dblobj = $this->get_membership_level_object( $id );
+        $categories = $this->get_membership_level_categories( $id );
 
         if ( ! empty( $dblobj ) ) {
             $this->ID = $dblobj->id;
@@ -56,11 +58,26 @@ class PMPro_Membership_Level{
             $this->allow_signups = $dblobj->allow_signups;
             $this->expiration_number = $dblobj->expiration_number;
             $this->expiration_period = $dblobj->expiration_period;
+            $this->categories = $categories;
         } else {
             return false;
         }
 
         return $this;
+    }
+
+    function get_membership_level_categories( $id ) {
+        global $wpdb;
+
+        $dblc = $wpdb->get_results( "SELECT * FROM $wpdb->pmpro_memberships_categories WHERE membership_id = " . $id . "", ARRAY_A );
+
+        $category_array = array();
+
+        foreach( $dblc as $category ) {
+            $category_array[] = intval( $category['category_id'] );
+        }
+        
+        return $category_array;
     }
 
     function get_membership_level_object( $id ) {
@@ -83,10 +100,20 @@ class PMPro_Membership_Level{
     function save() {
         global $wpdb;
 
+        if ( empty( $this->id ) ) {
+            $before_action = 'pmpro_add_membership_level';
+            $after_action = 'pmpro_added_membership_level';
+        } else {
+            $before_action = 'pmpro_update_membership_level';
+            $after_action = 'pmpro_updated_membership_level';
+        }
+
+        do_action( $before_action, $this );
+
         $wpdb->replace(
 			$wpdb->pmpro_membership_levels,
 			array(
-				'id'=>max($this->id, 0),
+				'id'=> $this->id,
 				'name' => $this->name,
 				'description' => $this->description,
 				'confirmation' => $this->confirmation,
@@ -117,7 +144,25 @@ class PMPro_Membership_Level{
 				'%s',		//expiration_period
 				'%d',		//allow_signups
 			)
-		);
+        );
+
+        if ( $wpdb->insert_id ) {
+           $this->id = $wpdb->insert_id;
+        }
+
+        // Drop all categories if there are categories set from $this.
+        if ( isset( $this->categories ) && is_array( $this->categories ) ) {
+            // Delete categories for membership ID.
+            $wpdb->delete( $wpdb->pmpro_memberships_categories, array('membership_id' => $this->id), array('%d') );
+
+            foreach( $this->categories as $key => $category ) {
+                if ( term_exists( get_cat_name( $category ), 'category' ) ) {
+                    $wpdb->insert( $wpdb->pmpro_memberships_categories, array( 'membership_id' => $this->id, 'category_id' => $category ), array( '%d', '%d' ) );
+                }
+            }
+        }
+        
+        do_action( $after_action, $this );
     }
 
 } // end of class
