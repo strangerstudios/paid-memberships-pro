43edb1ebded9fb3376a5696f4515b0258c860d88
Andrew Lima <andrew@arctek.co.za>
2020-03-19 16:04:19 +0200

    REST API V1 (MVP)
    REST API V1.

--- includes/rest-api.php
+++ includes/rest-api.php
@@ -146,6 +146,15 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 			)
 		));
 
+		register_rest_route( $pmpro_namespace, '/discount_code', 
+		array(
+			array(
+				'methods' => 'GET,POST,PUT,PATCH',
+				'callback' => array( $this, 'pmpro_rest_api_discount_code' ),
+				'permissions_callback' => array( $this, 'pmpro_rest_api_get_permissions_check' )
+			),
+		));
+
 		}
 		
 		/**
@@ -243,24 +252,9 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 			$params = $request->get_params();
 			$method = $request->get_method();
 
-			// Get all parameters.
-			$id = intval( $params['id'] );
-			$name = isset( $params['name'] ) ? sanitize_text_field( $params['name'] ) : '';
-			$description = isset( $params['description'] ) ? sanitize_text_field( $params['description'] ) : '';
-			$confirmation = isset( $params['confirmation'] ) ? sanitize_text_field( $params['confirmation'] ) : '';
-			$initial_payment = isset( $params['initial_payment'] ) ? floatval( $params['initial_payment'] ) : '';
-			$billing_amount = isset( $params['billing_amount'] ) ? floatval( $params['billing_amount'] ) : '';
-			$cycle_number = isset( $params['cycle_number'] ) ? intval( $params['cycle_number'] ) : '';
-			$cycle_period = isset( $params['cycle_period'] ) ? sanitize_text_field( $params['cycle_period'] ) : '';
-			$billing_limit = isset( $params['billing_limit'] ) ? sanitize_text_field( $params['billing_limit'] ) : '';
-			$trial_amount = isset( $params['trial_amount'] ) ? floatval( $params['trial_amount'] ) : '';
-			$trial_limit = isset( $params['trial_limit'] ) ? floatval( $params['trial_limit'] ) : '';
-			$allow_signups = isset( $params['allow_signups'] ) ? intval( $params['allow_signups'] ) : '';
-			$expiration_number = isset( $params['expiration_number'] ) ? intval( $params['expiration_number'] ) : '';
-			$expiration_period = isset( $params['expiration_period'] ) ? intval( $params['expiration_period'] ) : '';
-			$categories = isset( $params['categories'] ) ? sanitize_text_field( $params['categories'] ) : '';
+			$id = isset( $params['id'] ) ? intval( $params['id'] ) : '';
 
-			// Just return level object if method is GET, otherwise assume POST,PUT or PATCH.
+				// Just return level object if method is GET, otherwise assume POST,PUT or PATCH.
 			if ( $method === 'GET' ) {
 				return new PMPro_Membership_Level( $id );
 			} else {
@@ -268,71 +262,52 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 				if ( ! empty( $id ) && ( $method === 'PUT' || $method === 'PATCH' ) ) {
 					$level = new PMPro_Membership_Level( $id );
 				} elseif ( empty( $id ) && ( $method === 'PUT' || $method === 'PATCH' ) ) {
-					return false; // Error trying to update /// Improve this.
+					return false; // Error trying to update
+				} elseif ( $method === 'POST' ) {
+					$level = new PMPro_Membership_Level();
 				}
 
-				if ( $name ) {
-					$level->name = $name;
-				}
-				
-				if ( $description ) {
-					$level->description = $description;
-				}
+			$name = isset( $params['name'] ) ? sanitize_text_field( $params['name'] ) : $level->name;
+			$description = isset( $params['description'] ) ? sanitize_text_field( $params['description'] ) : $level->description;
+			$confirmation = isset( $params['confirmation'] ) ? sanitize_text_field( $params['confirmation'] ) : $level->confirmation;
+			$initial_payment = isset( $params['initial_payment'] ) ? floatval( $params['initial_payment'] ) : $level->initial_payment;
+			$billing_amount = isset( $params['billing_amount'] ) ? floatval( $params['billing_amount'] ) : $level->billing_amount;
+			$cycle_number = isset( $params['cycle_number'] ) ? intval( $params['cycle_number'] ) : $level->cycle_number;
+			$cycle_period = isset( $params['cycle_period'] ) ? sanitize_text_field( $params['cycle_period'] ) : $level->cycle_period;
+			$billing_limit = isset( $params['billing_limit'] ) ? sanitize_text_field( $params['billing_limit'] ) : $level->billing_limit;
+			$trial_amount = isset( $params['trial_amount'] ) ? floatval( $params['trial_amount'] ) : $level->trial_amount;
+			$trial_limit = isset( $params['trial_limit'] ) ? floatval( $params['trial_limit'] ) : $level->trial_limit;
+			$allow_signups = isset( $params['allow_signups'] ) ? intval( $params['allow_signups'] ) : $level->allow_signups;
+			$expiration_number = isset( $params['expiration_number'] ) ? intval( $params['expiration_number'] ) : $level->expiration_number;
+			$expiration_period = isset( $params['expiration_period'] ) ? intval( $params['expiration_period'] ) : $level->expiration_period;
+			$categories = isset( $params['categories'] ) ? PMPro_REST_API_Routes::pmpro_rest_api_convert_to_array( sanitize_text_field( $params['categories'] ) ) : $level->categories;
+			
+			// Set Level Object and save it.
+			$level->name = $name;
+			$level->description = $description;
+			$level->confirmation = $confirmation;
+			$level->initial_payment = $initial_payment;
+			$level->billing_amount = $billing_amount;
+			$level->cycle_number = $cycle_number;
+			$level->billing_limit = $billing_limit;
+			$level->trial_amount = $trial_amount;
+			$level->allow_signups = $allow_signups;
+			$level->expiration_number = $expiration_number;
+			$level->expiration_period = $expiration_period;
+			$level->categories = $categories;
+			$level->save();	
 
-				if ( $confirmation ) {
-					$level->confirmation = $confirmation;
-				}
-
-				if ( $initial_payment ) {
-					$level->initial_payment = $initial_payment;
-				}
-
-				if ( $billing_amount ) {
-					$level->billing_amount = $billing_amount;
-				}
-
-				if ( $cycle_number ) {
-					$level->cycle_number = $cycle_number;
-				}
-
-				if ( $cycle_period ) {
-					$level->cycle_period = $cycle_period;
-				}
-
-				if ( $billing_limit ) {
-					$level->billing_limit = $billing_limit;
-				}
-
-				if ( $trial_amount ) {
-					$level->trial_amount = $trial_amount;
-				}
-
-				if ( $trial_limit ) {
-					$level->trial_limit = $trial_limit;
-				}
-
-				if ( $allow_signups ) {
-					$level->allow_signups = $allow_signups;
-				}
-
-				if ( $expiration_number ) {
-					$level->expiration_number = $expiration_number;
-				}
-
-				if ( $expiration_period ) {
-					$level->expiration_period = $expiration_period;
-				}
-
-				if ( $categories ) {
-					$categories = explode( ',', $categories );
-					$level->categories = $categories;
-				}
-
-				$level->save();
-
-				return $level;
+			return $level;
 			}
 
+		}
+
+		/** 
+		 *	Helper function to convert comma separated items to an array.
+		 * @since 2.3
+		 */
+		function pmpro_rest_api_convert_to_array( $string ) {
+			return explode( ',', $string );
 		}
 
 		/**
@@ -353,6 +328,78 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 			return $level->delete();
 		}
 
+
+		/**
+		 * Retrieve/Create a discount code.
+		 * @since 2.3
+		 * Example: https://example.com/wp-json/pmpro/v1/discount_code
+		 */
+		function pmpro_rest_api_discount_code( $request ) {
+
+			if ( ! class_exists( 'PMPro_Discount_Code' ) ) {
+				return false;
+			}
+
+			$params = $request->get_params();
+			$method = $request->get_method();
+			$code = $params['code'];
+			$uses = $params['uses'];
+			$starts = $params['starts'];
+			$levels = $params['levels'];
+
+			// If it's a GET request, return the discount code object.
+			if ( $method == 'GET' ) {
+				return new PMPro_Discount_Code( $code );
+			}
+
+			if ( ! empty( $levels ) ) {
+				$levels = json_decode( $levels, true );
+
+				if ( is_array( $levels ) ) {
+					$levels_array = array();
+					foreach( $levels as $level ){
+						$levels_array[$level['level']] = array(
+							'initial_payment' => $level['initial_payment'],
+							'billing_amount' => $level['billing_amount'],
+							'cycle_number' => $level['cycle_number'],
+							'cycle_period' => $level['cycle_period'],
+							'billing_limit' => $level['cycle_period'],
+							'custom_trial' => $level['custom_trial'],
+							'trial_amount' => $level['trial_amount'],
+							'trial_limit' => $level['trial_limit'],
+							'expiration_number' => $level['expiration_number'],
+							'expiration_period' => $level['expiration_period']
+						);
+					}
+				}
+			}
+			
+			$discount_code = new PMPro_Discount_Code();
+
+			// See if code already exists when POSTING.
+			if ( $method == 'POST' && ! empty( $code ) ) {
+				// See if discount code exists.
+				if ( is_numeric( $code ) ) {
+					$discount_code->get_discount_code_by_id( $code );
+				} else {
+					$discount_code->get_discount_code_by_code( $code );
+				}
+
+				if ( ! empty( $discount_code->id ) ) {
+					return "Discount code already exists";
+				}
+			}
+
+			$discount_code->code = isset( $code ) ? sanitize_text_field( $code ) : '';
+			$discount_code->starts = isset( $starts ) ? sanitize_text_field( $starts ) : '';
+			$discount_code->ends = isset( $ends ) ? sanitize_text_field( $ends ) : '';
+			$discount_code->uses = isset( $uses ) ? intval( $uses ) : '';
+			$discount_code->levels = !empty( $levels_array ) ? $levels_array : $levels;
+			$discount_code->save();
+
+			return $discount_code;
+		}
+
 		/**
 		 * Default permissions check for endpoints/routes. Defaults to 'subscriber' for all GET requests and 'administrator' for any other type of request.
 		 * @since 2.3
@@ -368,7 +415,6 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 
 			$permissions = apply_filters( 'pmpro_rest_api_permissions', $permissions, $request );
 
-			$permissions = '';
 			return $permissions;
 		}
 
