3d58512c6be2785273dc5693854cbb5d323d9981
Jason Coleman <jason@strangerstudios.com>
2020-03-30 20:04:56 -0400

    Redirecting confirmation page to login or account page. Cleaned up WPCS and the logic here a bit.

--- preheaders/confirmation.php
+++ preheaders/confirmation.php
@@ -2,8 +2,16 @@
 
 global $current_user, $pmpro_invoice;
 
+// Get the membership level for the current user.
 if($current_user->ID)
     $current_user->membership_level = pmpro_getMembershipLevelForUser($current_user->ID);
+
+// Redirect to login if logged out.
+if ( ! is_user_logged_in() ) {
+    $redirect_url = pmpro_login_url();
+    wp_redirect( $redirect_url );
+    exit;
+} 
 
 /*
 	Use the filter to add your gateway here if you want to show them a message on the confirmation page while their checkout is pending.
@@ -11,13 +19,15 @@ if($current_user->ID)
 	In the meantime, the order is in pending status and the confirmation page shows a message RE waiting.
 */
 $gateways_with_pending_status = apply_filters('pmpro_gateways_with_pending_status', array('paypalstandard', 'twocheckout', 'gourl'));
-	
-//must be logged in
-if (empty($current_user->ID) || (empty($current_user->membership_level->ID) && !in_array(pmpro_getGateway(), $gateways_with_pending_status)))
-    wp_redirect(home_url());
+if ( ! pmpro_hasMembershipLevel() && ! in_array( pmpro_getGateway(), $gateways_with_pending_status ) ) {
+    // Logged in, but doesn't have a leve
+    $redirect_url = pmpro_url( 'account' );
+    wp_redirect( $redirect_url );
+    exit;
+}
 
-//if membership is a paying one, get invoice from DB
-if (!empty($current_user->membership_level) && !pmpro_isLevelFree($current_user->membership_level)) {
+// If membership is a paying one, get invoice from DB
+if ( ! empty( $current_user->membership_level ) && ! pmpro_isLevelFree( $current_user->membership_level ) ) {
     $pmpro_invoice = new MemberOrder();
-    $pmpro_invoice->getLastMemberOrder($current_user->ID, apply_filters("pmpro_confirmation_order_status", array("success", "pending")));
+    $pmpro_invoice->getLastMemberOrder( $current_user->ID, apply_filters( "pmpro_confirmation_order_status", array( "success", "pending" ) ) );
 }
