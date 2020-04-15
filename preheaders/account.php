34406599fd36f466edcc390a61c09c0c14ed7658
Jason Coleman <jason@strangerstudios.com>
2020-04-14 21:57:37 -0400

    Fixing parenthesis on this empty check

--- preheaders/account.php
+++ preheaders/account.php
@@ -24,7 +24,7 @@ if ( isset($_REQUEST['msg'] ) ) {
  * If not, and the site is using the pmpro_account_preheader_redirect
  * filter, redirect to that page.
  */
-if ( ! empty( $current_user->ID && empty( $current_user->membership_level->ID ) ) ) {
+if ( ! empty( $current_user->ID ) && empty( $current_user->membership_level->ID ) ) {
 	$redirect = apply_filters( 'pmpro_account_preheader_redirect', false );
 	if ( $redirect ) {
 		wp_redirect( $redirect );
