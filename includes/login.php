81fce4e95ef8a82a6e60039b5c3e7a769ac33664
Jason Coleman <jason@strangerstudios.com>
2020-04-16 19:19:30 -0400

    Sanitize even though we are escaping right away.

--- includes/login.php
+++ includes/login.php
@@ -416,7 +416,7 @@ function pmpro_reset_password_form() {
 			$error_codes = explode( ',', sanitize_text_field( $_REQUEST['error'] ) );
 		} ?>
 		<form name="resetpassform" id="resetpassform" class="pmpro_form" action="<?php echo esc_url( site_url( 'wp-login.php?action=resetpass' ) ); ?>" method="post" autocomplete="off">
-			<input type="hidden" id="user_login" name="rp_login" value="<?php echo esc_attr( $_REQUEST['login'] ); ?>" autocomplete="off" />
+			<input type="hidden" id="user_login" name="rp_login" value="<?php echo esc_attr( sanitize_text_field( $_REQUEST['login'] ) ); ?>" autocomplete="off" />
 			<input type="hidden" name="rp_key" value="<?php echo esc_attr( $_REQUEST['key'] ); ?>" />
 			<div class="pmpro_reset_password-fields">
 				<div class="pmpro_rest_password-field pmpro_rest_password-field-pass1">
