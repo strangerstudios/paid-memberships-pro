00c279d79d36159070e27be918519045d570ff2b
Andrew Lima <andrew@arctek.co.za>
2020-04-21 13:36:15 +0200

    Add name attribute to submit button.
    * Added name attribute to Update Profile button and fixed minor spacing issue with wrapped text.

--- includes/profile.php
+++ includes/profile.php
@@ -542,8 +542,8 @@ function pmpro_member_profile_edit_form() {
 			<input type="hidden" name="user_id" value="<?php echo $current_user->ID; ?>" />
 			<div class="pmpro_submit">
 				<hr />
-				<input type="submit" class="pmpro_btn pmpro_btn-submit" value="<?php _e('Update Profile', 'paid-memberships-pro' );?>" />
-				<input type="button" name="cancel" class="pmpro_btn pmpro_btn-cancel" value="<?php _e('Cancel', 'paid-memberships-pro' );?>" onclick="location.href='<?php echo pmpro_url( 'account'); ?>';" />
+				<input type="submit" name="submit" class="pmpro_btn pmpro_btn-submit" value="<?php _e( 'Update Profile', 'paid-memberships-pro' );?>" />
+				<input type="button" name="cancel" class="pmpro_btn pmpro_btn-cancel" value="<?php _e( 'Cancel', 'paid-memberships-pro' );?>" onclick="location.href='<?php echo pmpro_url( 'account'); ?>';" />
 			</div>
 		</form>
 	</div> <!-- end pmpro_member_profile_edit_wrap -->
