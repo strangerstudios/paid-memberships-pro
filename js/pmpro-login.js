304eaba982a016c09d747cfb0fd55e306c2ed9b4
Jason Coleman <jason@strangerstudios.com>
2020-04-24 06:17:12 -0400

    disable submit button if password is WEAK SAUCE

--- js/pmpro-login.js
+++ js/pmpro-login.js
@@ -10,6 +10,7 @@ jQuery(document).ready(function(){
 	function pmpro_check_password_strength( pass_field ) {
 		var pass1 = jQuery( pass_field ).val();		
 		var indicator = jQuery( '#pass-strength-result' );
+		var submitbutton = jQuery( '#resetpass-button' );
 		var strength;
 		if ( pass1 != '' ) {
 			strength = wp.passwordStrength.meter( pass1, wp.passwordStrength.userInputBlacklist(), pass1 );
@@ -22,21 +23,27 @@ jQuery(document).ready(function(){
 		switch ( strength ) {
 			case -1:
 				indicator.addClass( 'empty' ).html( '&nbsp;' );
+				submitbutton.prop( 'disabled', true );
 				break;
 			case 2:
 				indicator.addClass( 'bad' ).html( pwsL10n.bad );
+				submitbutton.prop( 'disabled', true );
 				break;
 			case 3:
 				indicator.addClass( 'good' ).html( pwsL10n.good );
+				submitbutton.prop( 'disabled', false );
 				break;
 			case 4:
 				indicator.addClass( 'strong' ).html( pwsL10n.strong );
+				submitbutton.prop( 'disabled', false );
 				break;
 			case 5:
 				indicator.addClass( 'short' ).html( pwsL10n.mismatch );
+				submitbutton.prop( 'disabled', false );
 				break;
 			default:
 				indicator.addClass( 'short' ).html( pwsL10n['short'] );
+				submitbutton.prop( 'disabled', true );
 		}
 	}
 	
