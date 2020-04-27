84df99a9bd8e04498f9eaf2c80703d482c643789
Kim Coleman <kim@strangerstudios.com>
2020-04-27 10:44:46 -0400

    Updating billing page to show a return link to membership account for paypal or check gateway view.

--- pages/billing.php
+++ pages/billing.php
@@ -90,11 +90,18 @@
 
 <?php if ( $has_recurring_levels ) {
 	if ( $show_check_payment_instructions ) {
-		$instructions = pmpro_getOption("instructions");
-		echo '<div class="pmpro_check_instructions">' . wpautop(wp_unslash( $instructions )) . '</div>';
-	} elseif ( $show_paypal_link ) { ?>
+		$instructions = pmpro_getOption("instructions"); ?>
+		<div class="pmpro_check_instructions"><?php echo wpautop( wp_unslash( $instructions ) ); ?></div>
+		<hr />
+		<p class="pmpro_actions_nav">
+			<span class="pmpro_actions_nav-right"><a href="<?php echo pmpro_url( 'account' )?>"><?php _e('View Your Membership Account &rarr;', 'paid-memberships-pro' );?></a></span>
+		</p> <!-- end pmpro_actions_nav -->
+	<?php } elseif ( $show_paypal_link ) { ?>
 		<p><?php  _e('Your payment subscription is managed by PayPal. Please <a href="http://www.paypal.com">login to PayPal here</a> to update your billing information.', 'paid-memberships-pro' );?></p>
-
+		<hr />
+		<p class="pmpro_actions_nav">
+			<span class="pmpro_actions_nav-right"><a href="<?php echo pmpro_url( 'account' )?>"><?php _e('View Your Membership Account &rarr;', 'paid-memberships-pro' );?></a></span>
+		</p> <!-- end pmpro_actions_nav -->
 	<?php } else { ?>
 		<div id="pmpro_level-<?php echo $level->id; ?>" class="<?php echo $pmpro_billing_gateway_class; ?>">
 		<form id="pmpro_form" class="pmpro_form" action="<?php echo pmpro_url("billing", "", "https")?>" method="post">
