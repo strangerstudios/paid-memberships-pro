58881b3f7cd28b13e64fb87c90f1ee004018417d
Kim Coleman <kim@strangerstudios.com>
2020-04-27 08:00:13 -0400

    Update to process cancellation for the recurring_subscription_failed txn_type

--- classes/gateways/class.pmprogateway_paypalexpress.php
+++ classes/gateways/class.pmprogateway_paypalexpress.php
@@ -763,7 +763,8 @@
 			// If we're processing an IPN request for this subscription, it's already cancelled at PayPal.
 			if (
 				( ! empty( $_POST['subscr_id'] ) && $_POST['subscr_id'] == $order->subscription_transaction_id ) ||
-				( ! empty( $_POST['recurring_payment_id'] ) && $_POST['recurring_payment_id'] == $order->subscription_transaction_id )
+				( ! empty( $_POST['recurring_payment_id'] ) && $_POST['recurring_payment_id'] == $order->subscription_transaction_id ) || 
+				( $_POST['txn_type'] != 'recurring_payment_failed' )
 			) {
 				return true;
 			}
