498c212221bc15d50967916d7621ad968d9e71b5
Jason Coleman <jason@strangerstudios.com>
2020-08-07 02:57:52 +1000

    hashing secret keys when storing and getting webhook ids.

--- classes/gateways/class.pmprogateway_stripe.php
+++ classes/gateways/class.pmprogateway_stripe.php
@@ -623,8 +623,9 @@ class PMProGateway_stripe extends PMProGateway {
 		
 		// If a secret key was passed in, return just the id for that key.
 		if ( ! empty( $secret_key ) ) {
-			if ( isset( $webhook_ids[$secret_key] ) ) {
-				return $webhook_ids[$secret_key];
+			$secret_key_hash = wp_hash( $secret_key );
+			if ( isset( $webhook_ids[$secret_key_hash] ) ) {
+				return $webhook_ids[$secret_key_hash];
 			} else {
 				return false;
 			}
@@ -651,12 +652,15 @@ class PMProGateway_stripe extends PMProGateway {
 			return false;
 		}
 		
+		// Hash the secret key so it's not left behind in the DB.
+		$secret_key_hash = wp_hash( $secret_key );
+		
 		$webhook_ids = self::get_webhook_ids();
 		
 		if ( ! empty( $webhook_id ) ) {
-			$webhook_ids[$secret_key] = $webhook_id;
+			$webhook_ids[$secret_key_hash] = $webhook_id;
 		} else {
-			unset( $webhook_ids[$secret_key] );
+			unset( $webhook_ids[$secret_key_hash] );
 		}
 		
 		update_option( 'pmpro_stripe_webhook_ids', $webhook_ids );
