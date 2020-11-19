109d07c8dccbd33211888ae6d805826f3433e913
Andrew Lima <andrew@arctek.co.za>
2020-11-19 13:46:40 +0200

    BUG FIX: reCAPTCHA not working
    BUG FIX: fixed priority in which reCAPTCHA priority was being loaded.

--- includes/recaptcha.php
+++ includes/recaptcha.php
@@ -28,91 +28,93 @@ function pmpro_init_recaptcha() {
 		
 		require_once(PMPRO_DIR . '/includes/lib/recaptchalib.php' );
 		
-		function pmpro_recaptcha_get_html ($pubkey, $error = null, $use_ssl = false) {
+		if ( ! function_exists( 'pmpro_recaptcha_get_html' ) ) {
+			function pmpro_recaptcha_get_html ($pubkey, $error = null, $use_ssl = false) {
 
-			// Figure out language.
-			$locale = get_locale();
-			if(!empty($locale)) {
-				$parts = explode("_", $locale);
-				$lang = $parts[0];
-			} else {
-				$lang = "en";	
-			}
-			$lang = apply_filters( 'pmpro_recaptcha_lang', $lang );
-
-			// Check which version of ReCAPTCHA we are using.
-			$recaptcha_version = pmpro_getOption( 'recaptcha_version' ); 
-
-			if( $recaptcha_version == '3_invisible' ) { ?>
-				<div class="g-recaptcha" data-sitekey="<?php echo $pubkey;?>" data-size="invisible" data-callback="onSubmit"></div>
-				<script type="text/javascript">															
-					var pmpro_recaptcha_validated = false;
-					var pmpro_recaptcha_onSubmit = function(token) {
-						if ( pmpro_recaptcha_validated ) {
-							jQuery('#pmpro_form').submit();
-							return;
-						} else {
-							jQuery.ajax({
-							url: '<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>',
-							type: 'GET',
-							timeout: 30000,
-							dataType: 'html',
-							data: {
-								'action': 'pmpro_validate_recaptcha',
-								'g-recaptcha-response': token,
-							},
-							error: function(xml){
-								alert('Error validating ReCAPTCHA.');
-							},
-							success: function(response){
-								if ( response == '1' ) {
-									pmpro_recaptcha_validated = true;
-									
-									//get a new token to be submitted with the form
-									grecaptcha.execute();
-								} else {
-									pmpro_recaptcha_validated = false;
-									
-									//warn user validation failed
-									alert( 'ReCAPTCHA validation failed. Try again.' );
-									
-									//get a new token to be submitted with the form
-									grecaptcha.execute();
+				// Figure out language.
+				$locale = get_locale();
+				if(!empty($locale)) {
+					$parts = explode("_", $locale);
+					$lang = $parts[0];
+				} else {
+					$lang = "en";	
+				}
+				$lang = apply_filters( 'pmpro_recaptcha_lang', $lang );
+	
+				// Check which version of ReCAPTCHA we are using.
+				$recaptcha_version = pmpro_getOption( 'recaptcha_version' ); 
+	
+				if( $recaptcha_version == '3_invisible' ) { ?>
+					<div class="g-recaptcha" data-sitekey="<?php echo $pubkey;?>" data-size="invisible" data-callback="onSubmit"></div>
+					<script type="text/javascript">															
+						var pmpro_recaptcha_validated = false;
+						var pmpro_recaptcha_onSubmit = function(token) {
+							if ( pmpro_recaptcha_validated ) {
+								jQuery('#pmpro_form').submit();
+								return;
+							} else {
+								jQuery.ajax({
+								url: '<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>',
+								type: 'GET',
+								timeout: 30000,
+								dataType: 'html',
+								data: {
+									'action': 'pmpro_validate_recaptcha',
+									'g-recaptcha-response': token,
+								},
+								error: function(xml){
+									alert('Error validating ReCAPTCHA.');
+								},
+								success: function(response){
+									if ( response == '1' ) {
+										pmpro_recaptcha_validated = true;
+										
+										//get a new token to be submitted with the form
+										grecaptcha.execute();
+									} else {
+										pmpro_recaptcha_validated = false;
+										
+										//warn user validation failed
+										alert( 'ReCAPTCHA validation failed. Try again.' );
+										
+										//get a new token to be submitted with the form
+										grecaptcha.execute();
+									}
 								}
-							}
-							});
-						}						
-	        		};
-
-					var pmpro_recaptcha_onloadCallback = function() {
-						// Render on main submit button.
-						grecaptcha.render('pmpro_btn-submit', {
-	            		'sitekey' : '<?php echo $pubkey;?>',
-	            		'callback' : pmpro_recaptcha_onSubmit
-	          			});
-						
-						// Update other submit buttons.
-						var submit_buttons = jQuery('.pmpro_btn-submit-checkout');
-						submit_buttons.each(function() {
-							if(jQuery(this).attr('id') != 'pmpro_btn-submit') {
-								jQuery(this).click(function(event) {
-									event.preventDefault();
-									grecaptcha.execute();
 								});
-							}
-						});
-	        		};
-	    		 </script>
-				 <script type="text/javascript"
-	 				src="https://www.google.com/recaptcha/api.js?onload=pmpro_recaptcha_onloadCallback&hl=<?php echo $lang;?>&render=explicit" async defer>
-	 			</script>
-			<?php } else { ?>
-				<div class="g-recaptcha" data-sitekey="<?php echo $pubkey;?>"></div>
-				<script type="text/javascript"
-					src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang;?>">
-				</script>
-			<?php }				
-		}
+							}						
+						};
+	
+						var pmpro_recaptcha_onloadCallback = function() {
+							// Render on main submit button.
+							grecaptcha.render('pmpro_btn-submit', {
+							'sitekey' : '<?php echo $pubkey;?>',
+							'callback' : pmpro_recaptcha_onSubmit
+							  });
+							
+							// Update other submit buttons.
+							var submit_buttons = jQuery('.pmpro_btn-submit-checkout');
+							submit_buttons.each(function() {
+								if(jQuery(this).attr('id') != 'pmpro_btn-submit') {
+									jQuery(this).click(function(event) {
+										event.preventDefault();
+										grecaptcha.execute();
+									});
+								}
+							});
+						};
+					 </script>
+					 <script type="text/javascript"
+						 src="https://www.google.com/recaptcha/api.js?onload=pmpro_recaptcha_onloadCallback&hl=<?php echo $lang;?>&render=explicit" async defer>
+					 </script>
+				<?php } else { ?>
+					<div class="g-recaptcha" data-sitekey="<?php echo $pubkey;?>"></div>
+					<script type="text/javascript"
+						src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang;?>">
+					</script>
+				<?php }				
+			}
+		}	
 		
 		//for templates using the old recaptcha_get_html
 		if( ! function_exists( 'recaptcha_get_html' ) ) {
@@ -125,7 +127,7 @@ function pmpro_init_recaptcha() {
 		$recaptcha_privatekey = pmpro_getOption( 'recaptcha_privatekey' );
 	}
 }
-add_action( 'wp', 'pmpro_init_recaptcha', 5 );
+add_action( 'wp', 'pmpro_init_recaptcha', 1 );
 
 /**
  * AJAX Method to Validate a ReCAPTCHA Response Token
