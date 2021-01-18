65c5f566217f18a217ab420e31c5d3bc68380840
Jason Coleman <jason@strangerstudios.com>
2021-01-18 13:51:56 +1100

    Adding select2 and field class from RH

--- paid-memberships-pro.php
+++ paid-memberships-pro.php
@@ -44,6 +44,7 @@ require_once( PMPRO_DIR . '/scheduled/crons.php' );                 // crons for
 
 require_once( PMPRO_DIR . '/classes/class.memberorder.php' );       // class to process and save orders
 require_once( PMPRO_DIR . '/classes/class.pmproemail.php' );        // setup and filter emails sent by PMPro
+require_once( PMPRO_DIR . '/classes/class-pmpro-field.php' );
 require_once( PMPRO_DIR . '/classes/class-pmpro-levels.php' );
 require_once( PMPRO_DIR . '/classes/class-pmpro-admin-activity-email.php' );        // setup the admin activity email
 
