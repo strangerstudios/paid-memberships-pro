1cab6cf6081fafa5ceeba22c1f6b3c18e4460989
Jason Coleman <jason@strangerstudios.com>
2021-03-10 06:54:24 +1100

    First pass fixing the membership block. Needs to save the state of the checkboxes to the block.

--- blocks/membership/block.js
+++ blocks/membership/block.js
@@ -13,9 +13,11 @@
 } = wp.blocks;
 const {
     PanelBody,
-    SelectControl,
+    CheckboxControl,
 } = wp.components;
-
+const {
+    useState,
+} = wp.element;
 const {
     InspectorControls,
     InnerBlocks,
@@ -49,33 +51,48 @@ const all_levels = [{ value: 0, label: "Non-Members" }].concat( pmpro.all_level_
              },
          },
          edit: props => {
-             const { attributes: {levels, uid}, setAttributes, isSelected } = props;
+             const { attributes: {levels, uid}, setAttributes, isSelected } = props;            
              if( uid=='' ) {
                var rand = Math.random()+"";
                setAttributes( { uid:rand } );
              }
+             
+             // Build an array of checkboxes for each level.
+             var checkboxes = all_levels.map(function(level) {
+                 const [ isChecked, setChecked ] = useState( level.value in levels );  
+                                  
+                 function setLevelsAttribute( checked ) {                     
+                     if ( checked && ! ( level.value in levels ) ) {
+                         // Add the level.                         
+                     } else if ( ! checked && level.value in levels ) {
+                         // Remove the level.                         
+                     }
+                                          
+                     setChecked( level.value );
+                     setAttributes( { levels } );                     
+                 }
+                 
+                 return [                    
+                    <CheckboxControl                    
+                        label = { level.label }
+                        checked = { isChecked }
+                        onChange = { setLevelsAttribute( isChecked ) }
+                    />
+                 ]
+             });
+             
              return [
                 isSelected && <InspectorControls>
-                    <PanelBody>
-                        <SelectControl
-                            multiple
-                            label={ __( 'Select levels to show content to:', 'paid-memberships-pro' ) }
-                            value={ levels }
-                            onChange={ levels => { setAttributes( { levels } ) } }
-                            options={ all_levels }
-                        />
+                    <PanelBody>                        
+                        <div class="pmpro-block-inspector-scrollable">
+                            {checkboxes}
+                        </div>
                     </PanelBody>
                 </InspectorControls>,
                 isSelected && <div className="pmpro-block-require-membership-element" >
                   <span className="pmpro-block-title">{ __( 'Require Membership', 'paid-memberships-pro' ) }</span>
-                  <PanelBody>
-                      <SelectControl
-                          multiple
-                          label={ __( 'Select levels to show content to:', 'paid-memberships-pro' ) }
-                          value={ levels }
-                          onChange={ levels => { setAttributes( { levels } ) } }
-                          options={ all_levels }
-                      />
+                  <PanelBody>                      
+                      {checkboxes}
                   </PanelBody>
                   <InnerBlocks
                       renderAppender={ () => (
