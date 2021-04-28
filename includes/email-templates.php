c543f8625d3f57574d94efc6935c621f75ed3b2b
Andrew Lima <andrew@arctek.co.za>
2021-04-28 14:40:33 +0200

    Add body parameter to defaults
    * ENHANCEMENT: Add body parameter to the email defaults.

--- includes/email-templates.php
+++ includes/email-templates.php
@@ -10,108 +10,140 @@
 $pmpro_email_templates_defaults = array(
 	'default'                  => array(
 		'subject'     => __( "An Email From !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __( 'Default Email', 'paid-memberships-pro')
+		'description' => __( 'Default Email', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_default_body')
 	),
 	'admin_change'             => array(
 		'subject'     => __( "Your membership at !!sitename!! has been changed", 'paid-memberships-pro' ),
-		'description' => __( 'Admin Change', 'paid-memberships-pro')
+		'description' => __( 'Admin Change', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_admin_change_body')
 	),
 	'admin_change_admin'       => array(
 		'subject'     => __( "Membership for !!user_login!! at !!sitename!! has been changed", 'paid-memberships-pro' ),
-		'description' => __('Admin Change (admin)', 'paid-memberships-pro')
+		'description' => __('Admin Change (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_admin_change_admin_body')
 	),
-	'billing'                  => array(
+	'billable_invoice' => array(
+		'subject' => __( 'Invoice for Order #: !!order_code!!', 'paid-memberships-pro' ),
+        'description' => __( 'Billable Invoice', 'paid-membershps-pro' ),
+		'body' => pmpro_getOption( 'email_billable_invoice' )
+	),
+	'billing'	=> array(
 		'subject'     => __( "Your billing information has been udpated at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Billing', 'paid-memberships-pro')
+		'description' => __('Billing', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_billing_body')
 	),
 	'billing_admin'            => array(
 		'subject'     => __( "Billing information has been udpated for !!user_login!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Billing (admin)', 'paid-memberships-pro')
+		'description' => __('Billing (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_billing_admin_body')
 	),
 	'billing_failure'          => array(
 		'subject'     => __( "Membership Payment Failed at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Billing Failure', 'paid-memberships-pro')
+		'description' => __('Billing Failure', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_billing_failure_body')
 	),
 	'billing_failure_admin'    => array(
 		'subject'     => __( "Membership Payment Failed For !!display_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Billing Failure (admin)', 'paid-memberships-pro')
+		'description' => __('Billing Failure (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_billing_failure_admin_body')
 	),
 	'cancel'                   => array(
 		'subject'     => __( "Your membership at !!sitename!! has been CANCELLED", 'paid-memberships-pro' ),
-		'description' => __('Cancel', 'paid-memberships-pro')
+		'description' => __('Cancel', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_cancel_body')
 	),
 	'cancel_admin'             => array(
 		'subject'     => __( "Membership for !!user_login!! at !!sitename!! has been CANCELLED", 'paid-memberships-pro' ),
-		'description' => __('Cancel (admin)', 'paid-memberships-pro')
+		'description' => __('Cancel (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_cancel_admin_body')
 	),
 	'checkout_check'           => array(
 		'subject'     => __( "Your membership confirmation for !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Check', 'paid-memberships-pro')
+		'description' => __('Checkout - Check', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_check_body')
 	),
 	'checkout_check_admin'     => array(
 		'subject'     => __( "Member Checkout for !!membership_level_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Check (admin)', 'paid-memberships-pro')
+		'description' => __('Checkout - Check (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_check_admin_body')
 	),
 	'checkout_express'         => array(
 		'subject'     => __( "Your membership confirmation for !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - PayPal Express', 'paid-memberships-pro')
+		'description' => __('Checkout - PayPal Express', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_express_body')
 	),
 	'checkout_express_admin'   => array(
 		'subject'     => __( "Member Checkout for !!membership_level_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - PayPal Express (admin)', 'paid-memberships-pro')
+		'description' => __('Checkout - PayPal Express (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_express_admin_body')
 	),
 	'checkout_free'            => array(
 		'subject'     => __( "Your membership confirmation for !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Free', 'paid-memberships-pro')
+		'description' => __('Checkout - Free', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_free_body')
 	),
 	'checkout_free_admin'      => array(
 		'subject'     => __( "Member Checkout for !!membership_level_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Free (admin)', 'paid-memberships-pro')
+		'description' => __('Checkout - Free (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_free_admin_body')
 	),
 	'checkout_freetrial'       => array(
 		'subject'     => __( "Your membership confirmation for !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Free Trial', 'paid-memberships-pro')
+		'description' => __('Checkout - Free Trial', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_freetrial_body')
 	),
 	'checkout_freetrial_admin' => array(
 		'subject'     => __( "Member Checkout for !!membership_level_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Free Trial (admin)', 'paid-memberships-pro')
+		'description' => __('Checkout - Free Trial (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_freetrial_admin_body')
 	),
 	'checkout_paid'            => array(
 		'subject'     => __( "Your membership confirmation for !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Paid', 'paid-memberships-pro')
+		'description' => __('Checkout - Paid', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_paid_body')
 	),
 	'checkout_paid_admin'      => array(
 		'subject'     => __( "Member Checkout for !!membership_level_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Paid (admin)', 'paid-memberships-pro')
+		'description' => __('Checkout - Paid (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_paid_admin_body')
 	),
 	'checkout_trial'           => array(
 		'subject'     => __( "Your membership confirmation for !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Trial', 'paid-memberships-pro')
+		'description' => __('Checkout - Trial', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_trial_body')
 	),
 	'checkout_trial_admin'     => array(
 		'subject'     => __( "Member Checkout for !!membership_level_name!! at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Checkout - Trial (admin)', 'paid-memberships-pro')
+		'description' => __('Checkout - Trial (admin)', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_checkout_trial_admin_body')
 	),
 	'credit_card_expiring'     => array(
 		'subject'     => __( "Credit Card on File Expiring Soon at !!sitename!!", 'paid-memberships-pro' ),
-		'description' => __('Credit Card Expiring', 'paid-memberships-pro')
+		'description' => __('Credit Card Expiring', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_credit_card_expiring_body')
 	),
 	'invoice'                  => array(
 		'subject'     => __( "INVOICE for !!sitename!! membership", 'paid-memberships-pro' ),
-		'description' => __('Invoice', 'paid-memberships-pro')
+		'description' => __('Invoice', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_invoice_body')
 	),
 	'membership_expired'       => array(
 		'subject'     => __( "Your membership at !!sitename!! has ended", 'paid-memberships-pro' ),
-		'description' => __('Membership Expired', 'paid-memberships-pro')
+		'description' => __('Membership Expired', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_membership_expired_body')
 	),
 	'membership_expiring'      => array(
 		'subject'     => __( "Your membership at !!sitename!! will end soon", 'paid-memberships-pro' ),
-		'description' => __('Membership Expiring', 'paid-memberships-pro')
+		'description' => __('Membership Expiring', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_membership_expiring_body')
 	),
 	'trial_ending'             => array(
 		'subject'     => __( "Your trial at !!sitename!! is ending soon", 'paid-memberships-pro' ),
-		'description' => __('Trial Ending', 'paid-memberships-pro')
+		'description' => __('Trial Ending', 'paid-memberships-pro'),
+		'body' => pmpro_getOption('email_trial_ending_body')
 	),
+
 );
 
 // add SCA payment action required emails if we're using PMPro 2.1 or later
