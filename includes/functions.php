2c5791dce2e90deb516acddd521e5b173aef900c
Jason Coleman <jason@strangerstudios.com>
2021-05-21 09:34:23 +1000

    Always set the order status to error if the old_level_status var is error.

--- includes/functions.php
+++ includes/functions.php
@@ -1116,10 +1116,10 @@ function pmpro_changeMembershipLevel( $level, $user_id = null, $old_level_status
 
 			if ( ! empty( $c_order->error ) ) {
 				$pmpro_error = $c_order->error;
-			} else {
-				if( $old_level_status == 'error' ) {
-					$c_order->updateStatus("error");
-				}
+			}
+			
+			if( $old_level_status == 'error' ) {
+				$c_order->updateStatus("error");
 			}
 		}
 	}
