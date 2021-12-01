f0b51d3b1278259da640705dda9f3578f493487a
Jarryd Long <JarrydLong@users.noreply.github.com>
2021-12-01 15:56:29 +0200

    Tidied up code, made more readable

--- adminpages/reports/sales.php
+++ adminpages/reports/sales.php
@@ -412,7 +412,14 @@ function pmpro_report_sales_page()
 					{ 'label': '<?php _e( 'Average*', 'paid-memberships-pro' );?>', 'type': 'number' },
 				],
 				<?php foreach($cols as $date => $value) { 
-					$tooltip_string = "<div style='padding: 10px'><p><strong>".$date."</strong></p><p><strong>". esc_html( sprintf( __( 'New %s', 'paid-memberships-pro' ), ucwords( $type ) ) ).'</strong>: '.pmpro_round_price( $value[1] ).'</p><p><strong>'.__('Recurring', 'paid-memberships-pro' ).'</strong>: '.pmpro_round_price( $value[0] - $value[1] ).'</p><p><strong>'.__('Total', 'paid-memberships-pro').'</strong>: '.pmpro_round_price( $value[0] ).'</div>';
+
+					$tooltip_string = "";
+					$tooltip_string .= "<div style='padding: 10px'>";
+					$tooltip_string .= "<p><strong>".$date."</strong></p>";
+					$tooltip_string .= "<p><strong>". esc_html( sprintf( __( "New %s", "paid-memberships-pro" ), ucwords( $type ) ) )."</strong>: ".pmpro_round_price( $value[1] )."</p>";
+					$tooltip_string .= "<p><strong>".__( "Recurring", "paid-memberships-pro" )."</strong>: ".pmpro_round_price( $value[0] - $value[1] )."</p>";
+					$tooltip_string .= "<p><strong>".__( "Total", "paid-memberships-pro" )."</strong>: ".pmpro_round_price( $value[0] )."</p>";
+					$tooltip_string .= "</div>";
 					?>
 					['<?php
 						if ( $period == "monthly" ) {
