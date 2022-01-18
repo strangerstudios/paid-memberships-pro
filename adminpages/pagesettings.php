edd76d2d216f3de0c7784161061d835ef2ee1105
Andrew Lima <andrew@arctek.co.za>
2022-01-18 14:36:16 +0200

    Backwards compatibility for sites pre 7.4
    BUG FIX: Fixed fatal error for sites running PHP version < 7.4 and navigating to the PMPro Page Settings.

--- adminpages/pagesettings.php
+++ adminpages/pagesettings.php
@@ -28,8 +28,16 @@ $extra_pages = apply_filters('pmpro_extra_page_settings', array());
 $post_types = apply_filters( 'pmpro_admin_pagesetting_post_type_array', array( 'page' ) );
 
 // For backward compatibility we extract the first element from the array
-$post_type = is_array( $post_types ) ? $post_types[ array_key_first( $post_types ) ] : $post_types;
-
+// For backward compatibility we extract the first element from the array
+if ( is_array( $post_types ) ) {
+    if ( function_exists( 'array_key_first' ) ) {
+        $post_type = $post_types[ array_key_first( $post_types ) ];
+    } else {
+        $post_type = $post_types[0];
+    }
+} else {
+    $post_type = $post_types;
+}
 /**
  * Set post type to use for PMPro pages in the page settings dropdown.
  *
