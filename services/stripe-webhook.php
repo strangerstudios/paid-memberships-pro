5037607fbf5c7cb08b1d6e3091807ef57e74afaa
dparker1005 <dlparker1005@gmail.com>
2022-05-20 10:45:01 -0400

    May fix error where card_... payment methods not passed with payment intent

--- services/stripe-webhook.php
+++ services/stripe-webhook.php
@@ -168,11 +168,12 @@
 						),
 					);
 					$payment_intent = \Stripe\PaymentIntent::retrieve( $payment_intent_args );
-					if ( empty( $payment_intent->payment_method ) ) {
+					$payment_method = $payment_intent->charges->data[0]->payment_method_details;
+					if ( empty( $payment_method ) ) {
 						$logstr .= "Could not find payment method for invoice " . $invoice->id;
 						pmpro_stripeWebhookExit();
 					}
-					pmpro_stripe_webhook_populate_order_from_payment( $morder, $payment_intent->payment_method );
+					pmpro_stripe_webhook_populate_order_from_payment( $morder, $payment_method );
 
 					//save
 					$morder->status = "success";
