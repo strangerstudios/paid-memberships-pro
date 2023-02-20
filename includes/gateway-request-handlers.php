4b57328c186f32c1a5a50dce8b3b11eb1eec6f1a
David Parker <dlparker1005@gmail.com>
2023-02-20 12:45:14 -0500

    Merged basic conpd logic. Still needs emails

--- includes/gateway-request-handlers.php
+++ includes/gateway-request-handlers.php
@@ -13,6 +13,8 @@
  * @return string Entry to add to IPN/webhook log.
  */
 function pmpro_handle_subscription_cancellation_at_gateway( $subscription_transaction_id, $gateway, $gateway_environment ) {
+	global $wpdb;
+
 	// Find subscription.
 	$subscription = PMPro_Subscription::get_subscription_from_subscription_transaction_id( $subscription_transaction_id, $gateway, $gateway_environment );
 	if ( empty( $subscription ) ) {
@@ -47,7 +49,10 @@ function pmpro_handle_subscription_cancellation_at_gateway( $subscription_transa
 		return 'We have already processed this cancellation. Probably originated from WP/PMPro. ( Subscription Transaction ID #' . $subscription_transaction_id . ')';
 	}
 
-	// Mark the PMPro_Subscription as cancelled.
+	// Store the old next payment date for the subscription for later.
+	$old_next_payment_date = $subscription->get_next_payment_date();
+
+	// Mark the PMPro_Subscription as cancelled (also clears the next payment date).
 	$subscription->set( 'status', 'cancelled' );
 	$subscription->save();
 
@@ -72,7 +77,36 @@ function pmpro_handle_subscription_cancellation_at_gateway( $subscription_transa
 		}
 	}
 
-	// Cancel the membership.
+	// Check if we want to try to extend the user's membership to the next payment date.
+	if ( apply_filters( 'pmpro_cancel_on_next_payment_date', true, $subscription->get_membership_level_id(), $user->ID ) ) {
+		// Check if $old_next_payment_date is in the future.
+		if ( ! empty( $old_next_payment_date ) && $old_next_payment_date > current_time( 'timestamp' ) ) {
+			// Extend the user's membership to the next payment date.
+			$enddate = date( 'Y-m-d H:i:s', $old_next_payment_date );
+			$wpdb->update(
+				$wpdb->pmpro_memberships_users,
+				[
+					'enddate' => $enddate,
+				],
+				[
+					'status'        => 'active',
+					'membership_id' => $subscription->get_membership_level_id(),
+					'user_id'       => $user->ID,
+				],
+				[
+					'%s',
+				],
+				[
+					'%s',
+					'%d',
+					'%d',
+				]
+			);
+			return 'Cancelled membership for user with id = ' . $user->ID . '. Subscription transaction id = ' . $subscription_transaction_id . '. Membership extended to next payment date.';
+		}
+	}
+
+	// We're not extending the user's membership to the next payment date, so cancel it now.
 	pmpro_cancelMembershipLevel( $subscription->get_membership_level_id(), $user->ID, 'cancelled' );
 
 	// Send an email to the member.
