26ae56ee0ee1c8ddeb69d10d55b0d2f21adef32e
David Parker <dlparker1005@gmail.com>
2023-02-24 12:21:37 -0500

    Simplifying pause mode checks

--- includes/functions.php
+++ includes/functions.php
@@ -4389,16 +4389,14 @@ function pmpro_compare_siteurl() {
 	$site_url = get_site_url();
 
 	$current_url = pmpro_getOption( 'last_known_url' );
-
-	if( empty( $current_url ) ) {
-		return false;
+	
+	// If we don't have a current URL yet, set it to the site URL.
+	if ( empty( $current_url ) ) {
+		pmpro_setOption( 'last_known_url', $site_url );
+		$current_url = $site_url;
 	}
 
-	if( $site_url !== $current_url ) {
-		return false;
-	}
-
-	return true;
+	return ( $site_url === $current_url );
 }
 
 /**
@@ -4408,14 +4406,14 @@ function pmpro_compare_siteurl() {
  * @return bool True if the the site is in pause mode
  */
 function pmpro_is_paused() {
-	$pause_mode = pmpro_getOption( 'pause_mode' );
-	
-	//We haven't saved the option or it isn't in pause mode
-	if( empty( $pause_mode ) || $pause_mode === false ) {
-		return false;
+	// If the current site URL is different than the last known URL, then we are in pause mode.
+	if ( ! pmpro_compare_siteurl() ) {
+		return true;
 	}
 
-	return true;
+	// Other pause conditions can eventually be added here.
+
+	return false;
 }
 
 /**
@@ -4423,8 +4421,10 @@ function pmpro_is_paused() {
  *
  * @param $state bool true or false if in pause mode state
  * @since 2.10
+ * @deprecated TBD No longer using `pmpro_pause_mode` option
  * @return bool True if the option has been updated
  */
 function pmpro_set_pause_mode( $state ) {
+	_deprecated_function( __FUNCTION__, 'TBD' );
 	return pmpro_setOption( 'pause_mode', $state );
 }
