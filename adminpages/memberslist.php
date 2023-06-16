227a20c34a50c09a5a6f02f7f720b68de3dc3de5
Jason Coleman <jason@strangerstudios.com>
2023-06-17 07:11:35 +1000

    Removed extra h1 section

--- adminpages/memberslist.php
+++ adminpages/memberslist.php
@@ -19,15 +19,11 @@ if ( isset( $_REQUEST['l'] ) ) {
 // Render the List Table.
 ?>
 	<hr class="wp-header-end">
-	<br class="wp-clearfix">
-	<h1 class="wp-heading-inline"><?php esc_html_e( 'Members List', 'paid-memberships-pro' ); ?></h1>
-	<a target="_blank" href="<?php echo esc_url( $csv_export_link ); ?>" class="page-title-action"><?php esc_html_e( 'Export to CSV', 'paid-memberships-pro' ); ?></a>
-	<?php do_action( 'pmpro_memberslist_before_table' ); ?>			
-	<form id="member-list-form" method="get">
-		<h1 class="wp-heading-inline"><?php esc_html_e( 'Members List', 'paid-memberships-pro' ); ?></h1>	
-		<a href="<?php echo esc_url( add_query_arg( array( 'page' => 'pmpro-members'), admin_url( 'admin.php' ) ) ); ?>" class="page-title-action pmpro-has-icon pmpro-has-icon-plus"><?php esc_html_e( 'Add New Member', 'paid-memberships-pro' ); ?></a>
-		<a target="_blank" href="<?php echo esc_url( $csv_export_link ); ?>" class="page-title-action pmpro-has-icon pmpro-has-icon-download"><?php esc_html_e( 'Export to CSV', 'paid-memberships-pro' ); ?></a>
-		<?php do_action( 'pmpro_memberslist_before_table' ); ?>	
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'Members List', 'paid-memberships-pro' ); ?></h1>	
+	<a href="<?php echo esc_url( add_query_arg( array( 'page' => 'pmpro-members'), admin_url( 'admin.php' ) ) ); ?>" class="page-title-action pmpro-has-icon pmpro-has-icon-plus"><?php esc_html_e( 'Add New Member', 'paid-memberships-pro' ); ?></a>
+	<a target="_blank" href="<?php echo esc_url( $csv_export_link ); ?>" class="page-title-action pmpro-has-icon pmpro-has-icon-download"><?php esc_html_e( 'Export to CSV', 'paid-memberships-pro' ); ?></a>
+	<?php do_action( 'pmpro_memberslist_before_table' ); ?>	
+	<form id="member-list-form" method="get">		
 		<input type="hidden" name="page" value="pmpro-memberslist" />
 		<?php
 			$user_list_table->search_box( __( 'Search Members', 'paid-memberships-pro' ), 'paid-memberships-pro' );
