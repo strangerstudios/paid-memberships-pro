062a81ac3d4f51b9a5a7f6256c2ae65164e2215a
Jason Coleman <jason@strangerstudios.com>
2023-07-12 11:36:44 +1000

    No longer using the CSS file. Removed the code to activate PMPro Courses. Going to adjust this.

--- includes/compatibility/lifter.php
+++ includes/compatibility/lifter.php
@@ -1,31 +1,15 @@
 <?php
 /**
- *  On the admin side check if lifters streamline feature is enabled and react accordingly.
+ * Compatibility for the LifterLMS plugin.
+ * We add an option to "Streamline LifterLMS" to the
+ * LifterLMS Setup Wizard and also the PMPro Advanced Settings Page.
+ * When enabled, this will:
+ * 1. Install, activate, and set up the PMPro Courses Add On.
+ * 2. Hide the LifterLMS Membership menu item from the admin dashboard.
+ * 3. Hide the Restrictions tab of the edit course page.
+ * 4. Hide the Access Plans section of the edit course page.
+ * 5. Override the My Memberships and My Orders tabs of the student dashboard.
  */
-function enable_streamlined_feature() {
-	$is_lifter_streamnlined_enabled = get_option( 'pmpro_lifter_streamline' ) == 'true';
-	if($is_lifter_streamnlined_enabled ) {
-		$lifter_streamline =  plugins_url() . '/paid-memberships-pro/css/lifter-streamline.css';
-		wp_register_style( 'pmpro_lifter', $lifter_streamline, [], PMPRO_VERSION, 'screen' );
-		wp_enqueue_style( 'pmpro_lifter' );
-		include_once( ABSPATH . 'wp-admin/includes/plugin-install.php' );
-	} else {
-		wp_dequeue_style( 'pmpro_lifter' );
-	}
-}
-add_action( 'admin_init','enable_streamlined_feature' );
-
-/**
- * On LifterLMS plugin activation, activate streamline feature and install PMPro Courses plugin.
- */
-function lifter_plugin_activation() {	
-	include_once( PMPRO_DIR . '/classes/' . 'class-quiet-plugin-installer.php' ) ;
-	$slug = 'pmpro-courses';
-	$installer = new Quiet_Skin(compact('title', 'url', 'nonce', 'plugin', 'api') );
-	$installer->download_install_and_activate($slug);	
-}
-register_activation_hook( 'lifterlms/lifterlms.php', 'lifter_plugin_activation' );
-
 /**
  * Add streamline setting to the PMPro Advanced Settings page.
  */
