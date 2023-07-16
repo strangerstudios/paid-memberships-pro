73ab627154ce05b32667c9be632d4bebf62dd521
Jason Coleman <jason@strangerstudios.com>
2023-07-17 00:31:20 +1000

    Moved pmpro-courses module code into our compat file here.

--- includes/compatibility/lifter.php
+++ includes/compatibility/lifter.php
@@ -4,12 +4,13 @@
  * We add an option to "Streamline LifterLMS" to the
  * LifterLMS Setup Wizard and also the PMPro Advanced Settings Page.
  * When enabled, this will:
- * 1. Install, activate, and set up the PMPro Courses Add On.
+ * 1. Allow LifterLMS courses to be restricted by PMPro levels.
  * 2. Hide the LifterLMS Membership menu item from the admin dashboard.
  * 3. Hide the Restrictions tab of the edit course page.
  * 4. Hide the Access Plans section of the edit course page.
  * 5. Override the My Memberships and My Orders tabs of the student dashboard.
  */
+
 /**
  * Add streamline setting to the PMPro Advanced Settings page.
  */
@@ -27,6 +28,138 @@ function pmpro_lifter_streamline_advanced_setting( $settings ) {
 	return $settings;
 }
 add_filter( 'pmpro_custom_advanced_settings', 'pmpro_lifter_streamline_advanced_setting' );
+
+/**
+ * Add Require Membership box to LifterLMS courses.
+ */
+function pmpro_lifter_admin_menu() {
+	// Bail if the streamline option is not enabled.
+	if ( ! get_option( 'pmpro_lifter_streamline' ) ) {
+		return;
+	}
+	
+	if( function_exists( 'pmpro_page_meta' ) ){
+		add_meta_box( 'pmpro_page_meta', esc_html__( 'Require Membership', 'pmpro-courses' ), 'pmpro_page_meta', 'course', 'side');
+	}
+}
+add_action('admin_menu', 'pmpro_lifter_admin_menu', 20);
+
+/**
+ * Override PMPro's the_content filter.
+ * We want to show course content even if it requires membership.
+ * Still showing the non-member text at the bottom.
+ */
+function pmpro_lifter_membership_content_filter( $filtered_content, $original_content ) {	
+	// Bail if the streamline option is not enabled.
+	if ( ! get_option( 'pmpro_lifter_streamline' ) ) {
+		return $filtered_content;
+	}
+	
+	if ( is_singular( 'course' ) ) {
+		// Show non-member text if needed.
+		ob_start();
+		// Get hasaccess ourselves so we get level ids and names.
+		$hasaccess = pmpro_has_membership_access(NULL, NULL, true);
+		if( is_array( $hasaccess ) ) {
+			//returned an array to give us the membership level values
+			$post_membership_levels_ids = $hasaccess[1];
+			$post_membership_levels_names = $hasaccess[2];
+			$hasaccess = $hasaccess[0];
+			if ( ! $hasaccess ) {
+				echo pmpro_get_no_access_message( '', $post_membership_levels_ids, $post_membership_levels_names );
+			}
+		}
+		
+		$after_the_content = ob_get_contents();
+		ob_end_clean();			
+		return $original_content . $after_the_content;		
+	} else {
+		return $filtered_content;	// Probably false.
+	}
+}
+add_filter( 'pmpro_membership_content_filter', 'pmpro_lifter_membership_content_filter', 10, 2 );
+
+/**
+ * Get courses associated with a level.
+ */
+function pmpro_lifter_get_courses_for_levels( $level_ids ) {
+	global $wpdb;
+	
+	// In case a level object was passed in.
+	if ( is_object( $level_ids ) ) {
+		$level_ids = $level_ids->ID;
+	}
+	
+	// Make sure we have an array of ids.
+	if ( ! is_array( $level_ids ) ) {
+		$level_ids = array( $level_ids );
+	}
+	
+	if ( empty( $level_ids ) ) {
+		return array();
+	}
+	
+	$course_ids = $wpdb->get_col(
+		$wpdb->prepare(
+			"
+				SELECT mp.page_id 
+				FROM $wpdb->pmpro_memberships_pages mp 
+				LEFT JOIN $wpdb->posts p ON mp.page_id = p.ID 
+				WHERE mp.membership_id IN(%s) 
+				AND p.post_type = 'course' 
+				AND p.post_status = 'publish' 
+				GROUP BY mp.page_id
+			",
+			implode(',', $level_ids )
+		)
+	);
+	
+	return $course_ids;
+}
+
+/**
+ * When users change levels, enroll/unenroll them from
+ * any associated private courses.
+ */
+function pmpro_lifter_after_all_membership_level_changes( $pmpro_old_user_levels ) {		
+	// Bail if the streamline option is not enabled.
+	if ( ! get_option( 'pmpro_lifter_streamline' ) ) {
+		return $pmpro_old_user_levels;
+	}
+	
+	foreach ( $pmpro_old_user_levels as $user_id => $old_levels ) {
+		// Get current courses.
+		$current_levels = pmpro_getMembershipLevelsForUser( $user_id );
+		if ( ! empty( $current_levels ) ) {
+			$current_levels = wp_list_pluck( $current_levels, 'ID' );
+		} else {
+			$current_levels = array();
+		}
+		$current_courses = pmpro_lifter_get_courses_for_levels( $current_levels );
+		
+		// Get old courses.
+		$old_levels = wp_list_pluck( $old_levels, 'ID' );
+		$old_courses = pmpro_lifter_get_courses_for_levels( $old_levels );
+		
+		// Unenroll the user in any courses they used to have, but lost.
+		$courses_to_unenroll = array_diff( $old_courses, $current_courses );
+		foreach( $courses_to_unenroll as $course_id ) {
+			if ( llms_is_user_enrolled( $user_id, $course_id ) ) {
+				// Unenroll student
+				llms_unenroll_student( $user_id, $course_id );					
+			}
+		}
+		
+		// Enroll the user in any courses for their current levels.
+		$courses_to_enroll = array_diff( $current_courses, $old_courses );
+		foreach( $courses_to_enroll as $course_id ) {
+			if ( ! llms_is_user_enrolled( $user_id, $course_id ) ) {
+				llms_enroll_student( $user_id, $course_id );
+			}
+		}
+	}
+}
+add_action( 'pmpro_after_all_membership_level_changes', 'pmpro_lifter_after_all_membership_level_changes' );
 
 /**
  * Hide the LifterLMS Membership menu item from the admin dashboard if streamline is enabled.
