ea395e19068cd8c369d45f9fb05194d124c603a9
David Parker <dlparker1005@gmail.com>
2023-07-19 11:31:05 -0400

    Updating build file

--- js/blocks/blocks.build.asset.php
+++ js/blocks/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '14045cd5bd5916038d8c');
+<?php return array('dependencies' => array('wp-element'), 'version' => '76703642acd3ec620ec0');
