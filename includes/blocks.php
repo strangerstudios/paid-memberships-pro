21603639a6184120711a8159cf0cbd88bb70d27d
Brian Santos <briancapellan10@gmail.com>
2023-08-09 13:36:28 -0400

    Converting account links section block

--- includes/blocks.php
+++ includes/blocks.php
@@ -30,6 +30,7 @@ add_filter( 'block_categories_all', 'pmpro_block_categories' );
 function pmpro_register_block_types() {
 	register_block_type( PMPRO_DIR . '/blocks/build/account-invoices-section' );
 	register_block_type( PMPRO_DIR . '/blocks/build/account-profile-section' );
+	register_block_type( PMPRO_DIR . '/blocks/build/account-links-section' );
 }
 add_action( 'init', 'pmpro_register_block_types' );
 
