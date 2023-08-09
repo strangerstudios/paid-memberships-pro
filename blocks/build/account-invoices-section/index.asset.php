d1c590145fbe947cb71ade274ff37bd90896018f
David Parker <dlparker1005@gmail.com>
2023-08-09 09:56:19 -0400

    Adding block props

--- blocks/build/account-invoices-section/index.asset.php
+++ blocks/build/account-invoices-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6f116e200c07da1108b4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3300d4c20f34af3e5619');
