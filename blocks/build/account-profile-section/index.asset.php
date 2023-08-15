c44c92993e3cb7bfbde1ea492d95719c11612d6e
Brian Santos <briancapellan10@gmail.com>
2023-08-15 11:51:48 -0400

    Converting account membership section block

--- blocks/build/account-profile-section/index.asset.php
+++ blocks/build/account-profile-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '78fdf6b09e9ee9c2844a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e985ed8f99176533a0f9');
