7482fae87d0a832d8e3f131ed252ebb9c0ead2e3
Brian Santos <briancapellan10@gmail.com>
2023-08-16 11:31:04 -0400

    Converting membership block

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,13 @@ config.entry = {
     "account-profile-section",
     "index.js"
   ),
+  "membership/index": path.resolve(
+    process.cwd(),
+    "blocks",
+    "src",
+    "membership",
+    "index.js"
+  ),
 };
 
 config.output = {
