fe0e2b0a6c90806cad4806128954058686dca878
David Parker <dlparker1005@gmail.com>
2023-09-01 14:28:56 -0400

    Updating missed files

--- blocks/build/checkout-button/index.asset.php
+++ blocks/build/checkout-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5e074b2e64ba8e635415');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6e5b1ce7a3534f6587a3');
