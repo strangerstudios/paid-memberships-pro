5162848d7468edfd1d1bbccb352bce235d0bd3cd
David Parker <dlparker1005@gmail.com>
2023-10-31 14:24:28 -0400

    Fix spacing

--- js/pmpro-admin.js
+++ js/pmpro-admin.js
@@ -962,23 +962,23 @@ function pmpro_changeTabs(e) {
 	e.preventDefault();
 	const target = e.target;
 	const parent = target.parentNode;
-  	const grandparent = parent.parentNode;
+	const grandparent = parent.parentNode;
 
-  // Remove all current selected tabs
-  parent
-    .querySelectorAll('[aria-selected="true"]')
-    .forEach((t) => t.setAttribute("aria-selected", false));
+	// Remove all current selected tabs
+	parent
+	.querySelectorAll('[aria-selected="true"]')
+	.forEach((t) => t.setAttribute("aria-selected", false));
 
-  // Set this tab as selected
-  target.setAttribute("aria-selected", true);
+	// Set this tab as selected
+	target.setAttribute("aria-selected", true);
 
-  // Hide all tab panels
-  grandparent
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((p) => p.setAttribute("hidden", true));
+	// Hide all tab panels
+	grandparent
+	.querySelectorAll('[role="tabpanel"]')
+	.forEach((p) => p.setAttribute("hidden", true));
 
-  // Show the selected panel
-  grandparent.parentNode
-    .querySelector(`#${target.getAttribute("aria-controls")}`)
-    .removeAttribute("hidden");
+	// Show the selected panel
+	grandparent.parentNode
+	.querySelector(`#${target.getAttribute("aria-controls")}`)
+	.removeAttribute("hidden");
 }
