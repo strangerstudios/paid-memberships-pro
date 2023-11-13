df75a968aa219a4c65e33cac2fa7d3d27c5862c1
Kim Coleman <kim@strangerstudios.com>
2023-11-13 13:04:04 -0500

    Fixing example display; not showing level block if no level is set

--- blocks/src/single-level/render.php
+++ blocks/src/single-level/render.php
@@ -2,4 +2,8 @@
 /**
  * Render the Single Level block on the frontend.
  */
+// Don't return if selected level is empty.
+if ( empty( $attributes['selected_membership_level'] ) ) {
+	return;
+}
 echo wp_kses_post( $content );
