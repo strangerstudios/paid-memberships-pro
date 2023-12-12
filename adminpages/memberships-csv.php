16a117e48808370f1759f73e325d590398848625
Andrew Lima <andrew@arctek.co.za>
2023-12-12 10:27:14 +0200

    remove PMPRO_BENCHMARK define by default
    * Remove "PMPRO_BENCHMARK" that defaulted to true. This was most likely defined during testing and we didn't remove it.

--- adminpages/memberships-csv.php
+++ adminpages/memberships-csv.php
@@ -5,8 +5,6 @@ if ( ! function_exists( 'current_user_can' ) || ( ! current_user_can( 'manage_op
 	die( __( 'You do not have permissions to perform this action.', 'paid-memberships-pro' ) );
 }
 
-define( 'PMPRO_BENCHMARK', true );
-
 if ( ! defined( 'PMPRO_BENCHMARK' ) ) {
 	define( 'PMPRO_BENCHMARK', false );
 }
