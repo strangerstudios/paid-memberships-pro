295da5fd5db3dee4e98bd757573f3ee0fed42dce
Jason Coleman <jason@strangerstudios.com>
2024-01-11 08:01:28 +1100

    changed this function name

--- js/pmpro-stripe.js
+++ js/pmpro-stripe.js
@@ -185,7 +185,7 @@ jQuery( document ).ready( function( $ ) {
 			jQuery.noConflict().ajax({
 				url: pmproStripe.restUrl + 'pmpro/v1/checkout_levels',
 				dataType: 'json',
-				data: pmpro_getNonSensitiveCheckoutFormData(),
+				data: pmpro_getCheckoutFormDataForCheckoutLevels(),
 				success: function(data) {
 					if ( data.hasOwnProperty('initial_payment') ) {
 						paymentRequest.update({
