fd476dbf7e2b9d1e337a5787679d231650d5f715
David Parker <dlparker1005@gmail.com>
2024-01-18 11:39:42 -0500

    Make spacing uniform

--- adminpages/membershiplevels.php
+++ adminpages/membershiplevels.php
@@ -1027,7 +1027,7 @@
 		                data = {
 		                    action: 'pmpro_update_level_order',
 		                    level_order: level_order,
-							nonce: '<?php echo wp_create_nonce( 'pmpro_update_level_order' ); ?>'
+		                    nonce: '<?php echo wp_create_nonce( 'pmpro_update_level_order' ); ?>'
 		                };
 
 		                $.post(ajaxurl, data, function(response) {
