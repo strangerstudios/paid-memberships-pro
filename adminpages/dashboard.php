d520892852e52e8cc8eedfb2976aa221461ab0b9
David Parker <dlparker1005@gmail.com>
2024-02-19 14:49:32 -0500

    Changing transient name

--- adminpages/dashboard.php
+++ adminpages/dashboard.php
@@ -230,13 +230,13 @@ function pmpro_dashboard_report_recent_members_callback() {
 	global $wpdb;
 
 	// Check if we have a cache.
-	$theusers = get_transient( 'pmpro_dashboard_report_recent_members_callback' );
+	$theusers = get_transient( 'pmpro_dashboard_report_recent_members' );
 	if ( empty( $theusers ) ) {
 		// No cached value. Get the users.
 		$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(CONVERT_TZ(u.user_registered, '+00:00', @@global.time_zone)) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP( CONVERT_TZ(mu.startdate, '+00:00', @@global.time_zone) ) as startdate, UNIX_TIMESTAMP( CONVERT_TZ(mu.enddate, '+00:00', @@global.time_zone) ) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0 AND mu.status = 'active' GROUP BY u.ID ORDER BY u.user_registered DESC LIMIT 5";
 		$sqlQuery = apply_filters( 'pmpro_members_list_sql', $sqlQuery );
 		$theusers = $wpdb->get_results( $sqlQuery );
-		set_transient( 'pmpro_dashboard_report_recent_members_callback', $theusers, 3600 * 24 );
+		set_transient( 'pmpro_dashboard_report_recent_members', $theusers, 3600 * 24 );
 	}
 	?>
     <span id="pmpro_report_members" class="pmpro_report-holder">
@@ -294,12 +294,12 @@ function pmpro_dashboard_report_recent_orders_callback() {
 	global $wpdb;
 
 	// Check if we have a cache.
-	$order_ids = get_transient( 'pmpro_dashboard_report_recent_orders_callback' );
+	$order_ids = get_transient( 'pmpro_dashboard_report_recent_orders' );
 	if ( empty( $order_ids ) ) {
 		// No cached value. Get the orders.
 		$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS id FROM $wpdb->pmpro_membership_orders ORDER BY id DESC, timestamp DESC LIMIT 5";
 		$order_ids = $wpdb->get_col( $sqlQuery );
-		set_transient( 'pmpro_dashboard_report_recent_orders_callback', $order_ids, 3600 * 24 );
+		set_transient( 'pmpro_dashboard_report_recent_orders', $order_ids, 3600 * 24 );
 	}
 	?>
     <span id="pmpro_report_orders" class="pmpro_report-holder">
@@ -431,8 +431,8 @@ function pmpro_dashboard_news_updates_callback() {
  * @since TBD
  */
 function pmpro_report_dashboard_delete_transients() {
-	delete_transient( 'pmpro_dashboard_report_recent_members_callback' );
-	delete_transient( 'pmpro_dashboard_report_recent_orders_callback' );
+	delete_transient( 'pmpro_dashboard_report_recent_members' );
+	delete_transient( 'pmpro_dashboard_report_recent_orders' );
 }
 add_action( 'pmpro_updated_order', 'pmpro_report_dashboard_delete_transients' );
 add_action( 'pmpro_after_checkout', 'pmpro_report_dashboard_delete_transients' );
