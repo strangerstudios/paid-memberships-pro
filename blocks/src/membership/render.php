c8f49ca03a09f0b001ffab1c0a4dbb48a9d11fad
Jason Coleman <jason@strangerstudios.com>
2024-03-22 17:46:51 -0400

    can't escape the inner blocsk

--- blocks/src/membership/render.php
+++ blocks/src/membership/render.php
@@ -2,4 +2,6 @@
 /**
  * Render the Content Visibility block on the frontend.
  */
-echo wp_kses_post( pmpro_apply_block_visibility( $attributes, $content ) );
+// The content here is the block's inner content, which should already be escaped.
+// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+echo pmpro_apply_block_visibility( $attributes, $content );
