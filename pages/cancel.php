870a43933a2e3fd7eba2b22b0155f900f0ce703d
Kim Coleman <kim@strangerstudios.com>
2024-07-10 20:56:13 -0400

    Adjusting the cancel page to have more clear messaging related to CONPD

--- pages/cancel.php
+++ pages/cancel.php
@@ -27,6 +27,7 @@ if(isset($_REQUEST['levelstocancel']) && $_REQUEST['levelstocancel'] !== 'all') 
 	$old_level_ids = false;
 }
 
+// Get the user's current levels.
 $user_levels = pmpro_getMembershipLevelsForUser( $current_user->ID );
 ?>
 <div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro' ) ); ?>">
@@ -46,14 +47,71 @@ $user_levels = pmpro_getMembershipLevelsForUser( $current_user->ID );
 						<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_card_content' ) ); ?>">
 							<p>
 								<?php
-								if ( is_string( $old_level_ids ) && $old_level_ids == 'all' ) {
-									esc_html_e( 'Are you sure you want to cancel your membership?', 'paid-memberships-pro' );
-								} else {
-									$level_names = $wpdb->get_col( "SELECT name FROM $wpdb->pmpro_membership_levels WHERE id IN('" . implode( "','", array_map( 'intval', $old_level_ids ) ) . "')" );
-									echo esc_html( sprintf( _n( 'Are you sure you want to cancel your %s membership?', 'Are you sure you want to cancel your %s memberships?', count( $level_names ), 'paid-memberships-pro' ), pmpro_implodeToEnglish( $level_names ) ) );
-								}
+									// Set up some variables for the message.
+									$show_notice_about_subscriptions = false;
+									$next_payment_date = false;
+
+									// Show a different message if they are cancelling all levels or specific level(s).
+									if ( is_string( $old_level_ids ) && $old_level_ids == 'all' ) {
+										// Do any of their levels have an active subscription?
+										$subscriptions =  PMPro_Subscription::get_subscriptions_for_user( $current_user->ID );
+										if ( ! empty( $subscriptions ) ) {
+											$show_notice_about_subscriptions = true;
+										}
+
+										if ( count( $user_levels ) > 1 ) {
+											esc_html_e( 'Are you sure you want to cancel all of your memberships?', 'paid-memberships-pro' );
+										} else {
+											esc_html_e( 'Are you sure you want to cancel your membership?', 'paid-memberships-pro' );
+
+											// Get the next payment date for their single level.
+											if ( $show_notice_about_subscriptions ) {
+												$subscription = $subscriptions[0];
+												$next_payment_date = $subscription->get_next_payment_date( get_option( 'date_format' ) );
+											}
+										}
+									} else {
+										$level_names = array_map(function($level_id) use ($user_levels) {
+											foreach ($user_levels as $level) {
+												if ($level->id == $level_id) {
+													return $level->name;
+												}
+											}
+										}, $old_level_ids);
+										echo esc_html( sprintf( _n( 'Are you sure you want to cancel your %s membership?', 'Are you sure you want to cancel your %s memberships?', count( $level_names ), 'paid-memberships-pro' ), pmpro_implodeToEnglish( $level_names ) ) );
+
+										// Do they have a subscription for any of the levels being cancelled?
+										$subscriptions = PMPro_Subscription::get_subscriptions_for_user( $current_user->ID, $old_level_ids );
+										if ( ! empty( $subscriptions ) ) {
+											$show_notice_about_subscriptions = true;
+
+											// Get the next payment date if they are cancelling a single level.
+											if ( count( $old_level_ids ) === 1 ) {
+												$subscription = $subscriptions[0];
+												$next_payment_date = $subscription->get_next_payment_date( get_option( 'date_format' ) );
+											}
+										}
+									}
 								?>
 							</p>
+
+							<?php
+								if ( $show_notice_about_subscriptions ) {
+									?>
+									<p>
+										<?php
+											if ( $next_payment_date ) {
+												// We have a single level with a subscription to cancel.
+												echo sprintf( esc_html__( 'Your subscription will be cancelled. You will not be billed again. Your membership will remain active until %s. ', 'paid-memberships-pro' ), esc_html( $next_payment_date ) );
+											} else {
+												// We have multiple levels with subscriptions to cancel.
+												esc_html_e( 'Some of the memberships you are cancelling have a recurring subscription. These subscriptions will be cancelled, you will not be billed again, and membership will remain active through the end of the current payment period.', 'paid-memberships-pro' );
+											}
+										?>
+									</p>
+									<?php			
+								}
+							?>
 
 							<?php
 							/**
@@ -69,10 +127,10 @@ $user_levels = pmpro_getMembershipLevelsForUser( $current_user->ID );
 
 							<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_form_submit' ) ); ?>">
 								<?php
-									if ( is_string( $old_level_ids ) && $old_level_ids == 'all' ) {
+									if ( is_string( $old_level_ids ) && $old_level_ids == 'all' && count( $user_levels ) > 1 ) {
 										$cancel_memberships_text = __( 'Yes, cancel all of my memberships', 'paid-memberships-pro' );
 										$keep_memberships_text = __( 'No, keep my memberships', 'paid-memberships-pro' );
-									} elseif ( count( $old_level_ids ) > 1 ) {
+									} elseif ( ! is_string( $old_level_ids ) && count( $old_level_ids ) > 1 ) {
 										$cancel_memberships_text = __( 'Yes, cancel these memberships', 'paid-memberships-pro' );
 										$keep_memberships_text = __( 'No, keep these memberships', 'paid-memberships-pro' );
 									} else {
@@ -98,11 +156,12 @@ $user_levels = pmpro_getMembershipLevelsForUser( $current_user->ID );
 							<h2 class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_card_title pmpro_font-large' ) ); ?>"><?php esc_html_e( 'My Memberships', 'paid-memberships-pro' ); ?></h2>
 							<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_card_content' ) ); ?>">
 								<p><?php esc_html_e( 'You have the following active memberships.', 'paid-memberships-pro' ); ?></p>
-								<table class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table' ) ); ?>">
+								<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_spacer' ) ); ?>"></div>
+								<table class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table pmpro_table_cancel', 'pmpro_table_cancel' ) ); ?>">
 									<thead>
 										<tr>
-											<th><?php esc_html_e("Level", 'paid-memberships-pro' );?></th>
-											<th><?php esc_html_e("Expiration", 'paid-memberships-pro' ); ?></th>
+											<th class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table_cancel-level' ) ); ?>"><?php esc_html_e("Level", 'paid-memberships-pro' );?></th>
+											<th class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table_cancel-expiration' ) ); ?>"><?php esc_html_e("Expiration", 'paid-memberships-pro' ); ?></th>
 											<th></th>
 										</tr>
 									</thead>
@@ -111,15 +170,15 @@ $user_levels = pmpro_getMembershipLevelsForUser( $current_user->ID );
 											foreach ( $user_levels as $level ) {
 											?>
 											<tr>
-												<th class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_cancel-membership-levelname' ) ); ?>">
+												<th class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table_cancel-level' ) ); ?>" data-title="<?php esc_attr_e( 'Level', 'paid-memberships-pro' ); ?>">
 													<?php echo esc_html( $level->name );?>
 												</th>
-												<td class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_cancel-membership-expiration' ) ); ?>">
+												<td class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table_cancel-expiration' ) ); ?>" data-title="<?php esc_attr_e( 'Expiration', 'paid-memberships-pro' ); ?>">
 												<?php
 													echo wp_kses_post( pmpro_get_membership_expiration_text( $level, $current_user ) );
 												?>
 												</td>
-												<td class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_cancel-membership-cancel' ) ); ?>">
+												<td class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_table_cancel-action' ) ); ?>">
 													<a href="<?php echo esc_url( pmpro_url( "cancel", "?levelstocancel=" . $level->id ) ) ?>"><?php esc_html_e("Cancel", 'paid-memberships-pro' );?></a>
 												</td>
 											</tr>
@@ -129,9 +188,16 @@ $user_levels = pmpro_getMembershipLevelsForUser( $current_user->ID );
 									</tbody>
 								</table>
 							</div> <!-- end pmpro_card_content -->
-							<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_card_actions' ) ); ?>">
-								<a href="<?php echo esc_url( pmpro_url( "cancel", "?levelstocancel=all" ) ); ?>"><?php esc_html_e("Cancel All Memberships", 'paid-memberships-pro' );?></a>
-							</div> <!-- end pmpro_card_actions -->
+							<?php
+								// Show a link to cancel all memberships if the user has more than one.
+								if ( count( $user_levels ) > 1 ) {
+									?>
+									<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_card_actions' ) ); ?>">
+										<a href="<?php echo esc_url( pmpro_url( "cancel", "?levelstocancel=all" ) ); ?>"><?php esc_html_e("Cancel All Memberships", 'paid-memberships-pro' );?></a>
+									</div> <!-- end pmpro_card_actions -->
+									<?php
+								}
+							?>
 						</div> <!-- end pmpro_card -->
 						<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_spacer' ) ); ?>"></div>
 						<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro_actions_nav' ) ); ?>">
