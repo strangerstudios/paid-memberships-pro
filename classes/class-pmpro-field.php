a88ca7ee3908af106b753b5312c0c9a0bb2db06b
Andrew Lima <andrew@arctek.co.za>
2024-09-11 14:14:51 +0200

    Only load default value at checkout.
    * ENHANCEMENT: Only load the default value when a customer is on the checkout page.

--- classes/class-pmpro-field.php
+++ classes/class-pmpro-field.php
@@ -650,7 +650,7 @@ class PMPro_Field {
 			$r_end .= "</div>";
 		}
 
-		if ( empty( $value ) ) {
+		if ( empty( $value ) && pmpro_is_checkout() ) {
 			/**
 			 * Filter to set the default value for a field. The default value will only load if no value is already found.
 			 * 
