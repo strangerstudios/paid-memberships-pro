7689d154fcb244fe5c06301e0a5d862b17442f83
David Parker <dlparker1005@gmail.com>
2025-02-13 08:39:05 -0500

    Fixing spacing for the default body for the checkout paid email

--- classes/email-templates/class-pmpro-email-template-checkout-paid.php
+++ classes/email-templates/class-pmpro-email-template-checkout-paid.php
@@ -86,29 +86,29 @@ class PMPro_Email_Template_Checkout_Paid extends PMPro_Email_Template {
 	 */
 	public static function get_default_body() {
 		return  wp_kses_post( '<p>Thank you for your membership to !!sitename!!. Your membership account is now active.</p>
-		!!membership_level_confirmation_message!!
-		<p>Below are details about your membership account and a receipt for your initial membership order.</p>
-		
-		<p>Account: !!display_name!! (!!user_email!!)</p>
-		<p>Membership Level: !!membership_level_name!!</p>
-		<p>Membership Fee: !!membership_cost!!</p>
-		!!membership_expiration!! !!discount_code!!
-		
-		<p>
-			Order #!!order_id!! on !!order_date!!<br />
-			Total Billed: !!order_total!!
-		</p>
-		<p>
-			Billing Information:<br />
-			!!billing_address!!
-		</p>
-		
-		<p>
-			!!cardtype!!: !!accountnumber!!<br />
-			Expires: !!expirationmonth!!/!!expirationyear!!
-		</p>
-		
-		<p>Log in to your membership account here: !!login_url!!</p>', 'paid-memberships-pro' );
+!!membership_level_confirmation_message!!
+<p>Below are details about your membership account and a receipt for your initial membership order.</p>
+
+<p>Account: !!display_name!! (!!user_email!!)</p>
+<p>Membership Level: !!membership_level_name!!</p>
+<p>Membership Fee: !!membership_cost!!</p>
+!!membership_expiration!! !!discount_code!!
+
+<p>
+	Order #!!order_id!! on !!order_date!!<br />
+	Total Billed: !!order_total!!
+</p>
+<p>
+	Billing Information:<br />
+	!!billing_address!!
+</p>
+
+<p>
+	!!cardtype!!: !!accountnumber!!<br />
+	Expires: !!expirationmonth!!/!!expirationyear!!
+</p>
+
+<p>Log in to your membership account here: !!login_url!!</p>', 'paid-memberships-pro' );
 	}
 
 	/**
