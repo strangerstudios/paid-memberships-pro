931ac61e94df8cec061983907c62eb86a05db4e0
Andrew Lima <andrew@arctek.co.za>
2025-03-19 17:16:43 +0200

    Refactored the logic around this.

--- includes/compatibility/bricks.php
+++ includes/compatibility/bricks.php
@@ -44,55 +44,61 @@ function pmpro_bricks_add_condition_group( $groups ) {
  * @return array The modified condition options.
  */
 function pmpro_bricks_add_custom_condition( $options ) {
+	// All Members
 	$options[] = array(
-		'key'   => 'pmpro_membership_level',
-		'label' => esc_html__( 'PMPro Membership Level', 'paid-memberships-pro' ),
+		'key'   => 'pmpro_membership_level_all_members',
+		'label' => esc_html__( 'All Members', 'paid-memberships-pro' ),
 		'group' => 'pmpro',
 		'compare' => array(
 			'type'        => 'select',
 			'options'     => array(
-				'==' => esc_html__( 'Show content to', 'paid-memberships-pro' ),
-				'!=' => esc_html__( 'Hide content from', 'paid-memberships-pro' ),
+				'==' => esc_html__( 'Show content to All Members', 'paid-memberships-pro' ),
+				'!=' => esc_html__( 'Hide content from All Members', 'paid-memberships-pro' ),
+			),
+			'placeholder' => esc_html__( 'Select Comparison', 'paid-memberships-pro' ),
+		),
+		'value'   => array(
+		),
+	);
+
+	// Show for specific members.
+	$options[] = array(
+		'key'   => 'pmpro_membership_level_specific_levels',
+		'label' => esc_html__( 'Specific Membership Levels', 'paid-memberships-pro' ),
+		'group' => 'pmpro',
+		'compare' => array(
+			'type'        => 'select',
+			'options'     => array(
+				'==' => esc_html__( 'Show content to Specific Membership Levels', 'paid-memberships-pro' ),
+				'!=' => esc_html__( 'Hide content from Specific Membership Levels', 'paid-memberships-pro' ),
 			),
 			'placeholder' => esc_html__( 'Select Comparison', 'paid-memberships-pro' ),
 		),
 		'value'   => array(
 			'type'        => 'select',
-			'options'     => pmpro_bricks_get_membership_levels(),
+			'options'     => wp_list_pluck( pmpro_getAllLevels( true ), 'name', 'id' ),
+			'multiple'    => true,
+		),
+	);
+
+	// Non-members
+	$options[] = array(
+		'key'   => 'pmpro_membership_level_logged_in_users',
+		'label' => esc_html__( 'Logged-in users', 'paid-memberships-pro' ),
+		'group' => 'pmpro',
+		'compare' => array(
+			'type'        => 'select',
+			'options'     => array(
+				'==' => esc_html__( 'Show content to Logged-in users', 'paid-memberships-pro' ),
+				'!=' => esc_html__( 'Hide content from Logged-in users', 'paid-memberships-pro' ),
+			),
+			'placeholder' => esc_html__( 'Select Comparison', 'paid-memberships-pro' ),
+		),
+		'value'   => array(
 		),
 	);
 
 	return $options;
-}
-
-/**
- * Get the membership levels for the PMPro condition.
- *
- * Retrieves all PMPro membership levels and structures them for the Bricks Builder dropdown.
- * Includes a "Non-Members" option for users who are not members.
- *
- * @since TBD
- *
- * @see pmpro_getAllLevels()
- * @return array The membership levels formatted for Bricks conditions.
- */
-function pmpro_bricks_get_membership_levels() {
-	$pmpro_levels = pmpro_getAllLevels( true );
-
-	$levels_options_array = array();
-
-	// Add some custom levels_options_array/values to the level ID array.
-	$levels_options_array[-1] = esc_html__( 'All Members', 'paid-memberships-pro' ); // This gets pushed to the bottom no matter what we do. TODO: Bricks must fix this.
-	$levels_options_array[0] = esc_html__( 'Non Members', 'paid-memberships-pro' );
-
-	// Add the membership levels to the levels_options_array.
-	if ( ! empty( $pmpro_levels ) ) {
-		foreach ( $pmpro_levels as $pmpro_level ) {
-			$levels_options_array[ (int) $pmpro_level->id ] = esc_html( $pmpro_level->name );
-		}
-	};
-
-	return $levels_options_array;
 }
 
 /**
@@ -110,63 +116,35 @@ function pmpro_bricks_get_membership_levels() {
  * @return bool The modified result based on the membership level comparison.
  */
 function pmpro_bricks_condition_result( $result, $condition_key, $condition ) {
-	if ( $condition_key !== 'pmpro_membership_level' ) {
+
+	// Check if the condition is a pmpro_ prefixed condition. If not let's bail.
+	if ( strpos( $condition_key, 'pmpro_' ) !== 0 ) {
 		return $result;
 	}
+	
+	// Set up some default variables here.
+	$comparison = 1;
+	$condition_operator = $condition['compare'];
+	$has_access = false;
 
-	// Get the conditional comparison operator.
-	$compare = '==';
-	if ( isset( $condition['compare'] ) ) {
-		$compare = $condition['compare'];
+	// Check for all members.
+	if ( $condition_key === 'pmpro_membership_level_all_members' ) {
+		$user_value = pmpro_hasMembershipLevel();
+		$has_access = pmpro_int_compare( $user_value, $comparison, $condition_operator );
 	}
 
-	// Get the level ID of the condition.
-	$condition_level_id = '';
-	if ( isset( $condition['value'] ) ) {
-		$condition_level_id = $condition['value'];
+	// Check for logged-in non-members.
+	if ( $condition_key === 'pmpro_membership_level_logged_in_users' ) {
+		$user_value = ! pmpro_hasMembershipLevel();
+		$has_access = pmpro_int_compare( $user_value, $comparison, $condition_operator );
+	}
+	
+	// Check for specific membership levels.
+	if ( $condition_key === 'pmpro_membership_level_specific_levels' ) {
+		$user_value = pmpro_hasMembershipLevel( $condition['value'] );
+		$has_access = pmpro_int_compare( $user_value, $comparison, $condition_operator );
 	}
 
-	// Figure out if the member has the relevant condition.
-	return pmpro_bricks_condition_checker( $condition_level_id, $compare );
-}
-
-/**
- * Helper function to figure out if the condition is met or not.
- *
- * @since TBD
- * 
- * @param string $condition_level_id The level ID to check against.
- * @param string $compare The comparison operator, in this case it's either '==' or '!='.
- * @return boolean $condition_met Whether the condition is met.
- */
-function pmpro_bricks_condition_checker( $condition_level_id, $compare ) {
-
-	// Always cast to an integer, to be safe.
-	$condition_level_id = (int) $condition_level_id;
-
-	if ( $condition_level_id > 0 ) {
-		$user_has_level = pmpro_hasMembershipLevel( $condition_level_id );
-	} elseif ( $condition_level_id === 0 ) {
-		$user_has_level = ! pmpro_hasMembershipLevel();
-	} elseif ( $condition_level_id === -1 ) {
-		$user_has_level = pmpro_hasMembershipLevel();
-	}
-
-	// Compare the condition we're looking for.
-	if ( $compare === '==' ) {
-		$condition_met = $user_has_level;
-	} elseif ( $compare === '!=' ) {
-		$condition_met = ! $user_has_level;
-	}
-
-	/**
-	 * Filter to allow bypassing of the condition check for Bricks Builder condition.
-	 * 
-	 * @since TBD
-	 * 
-	 * @param boolean $condition_met Whether the condition is met.
-	 * @param string $condition_level_id The level ID to check against.
-	 * @param string $compare The comparison operator.
-	 */
-	return apply_filters( 'pmpro_bricks_condition_access', $condition_met, $condition_level_id, $compare );
+	// Return the access to the bricks builder. To filter this, use the `pmpro_has_membership_level` filter.
+	return $has_access;
 }
