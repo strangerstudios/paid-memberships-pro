9a1a38818ed60ec6f3167f884943addc8515f735
Mirco Babini <mirkolofio@gmail.com>
2025-04-25 23:31:08 +0200

    Ensure we work with https urls, to standardize comparison.

--- includes/updates/upgrade_3_4_7.php
+++ includes/updates/upgrade_3_4_7.php
@@ -13,6 +13,8 @@ function pmpro_upgrade_3_4_7() {
 	if ( ! empty( $current_url_hash ) ) {
 		// Check if current value was sha256 encoded; if not, encode it.
 		if ( ! preg_match( '/^[a-f0-9]{64}$/', $current_url_hash ) ) {
+			// Ensure we work with https urls, to standardize comparison.
+			$current_url_hash = str_replace( 'http://', 'https://', $current_url_hash );
 			$current_url_hash = hash( 'sha256', $current_url_hash );
 			update_option( 'pmpro_last_known_url', $current_url_hash );
 		}
