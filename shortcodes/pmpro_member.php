8b7d51f3141691542ad534ebca391343f90a4a65
David Parker <dlparker1005@gmail.com>
2025-06-09 13:02:51 -0400

    Simplifying level meta checks

--- shortcodes/pmpro_member.php
+++ shortcodes/pmpro_member.php
@@ -43,11 +43,6 @@ function pmpro_member_shortcode( $atts, $content = null, $shortcode_tag = '' ) {
 		'level_cost',
 	);
 
-	// Get a list of membership level meta fields.
-	global $wpdb;
-	$sqlQuery = "SELECT DISTINCT meta_key FROM $wpdb->pmpro_membership_levelmeta";
-	$pmpro_level_meta_fields = $wpdb->get_col( $sqlQuery );
-
 	// Get a list of fields related to the user's subscription.
 	$pmpro_subscription_fields = array(
 		'membership_billing_amount',
@@ -104,7 +99,7 @@ function pmpro_member_shortcode( $atts, $content = null, $shortcode_tag = '' ) {
 		'trial_amount',
 	);
 
-	if ( in_array( $field, $pmpro_level_fields ) || in_array( $field, $pmpro_subscription_fields ) || in_array( $field, $pmpro_level_meta_fields ) ) {
+	if ( in_array( $field, $pmpro_level_fields ) || in_array( $field, $pmpro_subscription_fields ) || 'level_meta_' === substr( $field, 0, 11 ) ) {
 		// Fields about the user's membership or subscription.
 		// Get the membership level to show.
 		$membership_level = null;
@@ -143,9 +138,9 @@ function pmpro_member_shortcode( $atts, $content = null, $shortcode_tag = '' ) {
 				$field = str_replace( 'membership_', '', $field );
 				$r     = $membership_level->{$field};
 			}
-		} elseif( in_array( $field, $pmpro_level_meta_fields ) ) {
+		} elseif( 'level_meta_' === substr( $field, 0, 11 ) ) {
 			// Membership level meta fields.
-			$r = get_pmpro_membership_level_meta( $membership_level->id, $field, true );
+			$r = get_pmpro_membership_level_meta( $membership_level->id, substr( $field, 11 ), true );
 		} else {
 			// Subscription fields.
 			$subscriptions = PMPro_Subscription::get_subscriptions_for_user( $user_id, $membership_level->id );
