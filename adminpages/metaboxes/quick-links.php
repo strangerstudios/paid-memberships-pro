14d0da5e36cdeef265b4f699ec3c6e025901506f
Dale Mugford <dmugford@paidmembershipspro.com>
2025-06-19 16:21:22 -0400

    Entab; WPCS formatting
    Signed-off-by: Dale Mugford <dmugford@paidmembershipspro.com>

--- adminpages/metaboxes/quick-links.php
+++ adminpages/metaboxes/quick-links.php
@@ -19,16 +19,22 @@ function pmpro_dashboard_quick_links_callback() {
 			'href'        => add_query_arg( array( 'page' => 'pmpro-member' ), admin_url( 'admin.php' ) ),
 			'title'       => __( 'Add a Member', 'paid-memberships-pro' ),
 			'description' => __( 'Add membership to a new or existing user.', 'paid-memberships-pro' ),
-			'icon'  => 'user-plus',
+			'icon'        => 'user-plus',
 		);
 	}
 
 	if ( current_user_can( 'pmpro_discountcodes' ) ) {
 		$quick_lnks[] = array(
-			'href'        => add_query_arg( array( 'page' => 'pmpro-discountcodes', 'edit' => '-1' ), admin_url( 'admin.php' ) ),
+			'href'        => add_query_arg(
+				array(
+					'page' => 'pmpro-discountcodes',
+					'edit' => '-1',
+				),
+				admin_url( 'admin.php' )
+			),
 			'title'       => __( 'Create a Discount Code', 'paid-memberships-pro' ),
 			'description' => __( 'Create a new discount code for your members.', 'paid-memberships-pro' ),
-			'icon'  => 'tag',
+			'icon'        => 'tag',
 		);
 	}
 
@@ -37,7 +43,7 @@ function pmpro_dashboard_quick_links_callback() {
 			'href'        => add_query_arg( array( 'page' => 'pmpro-emailtemplates' ), admin_url( 'admin.php' ) ),
 			'title'       => __( 'Edit Email Templates', 'paid-memberships-pro' ),
 			'description' => __( 'Manage your email templates for member communications.', 'paid-memberships-pro' ),
-			'icon'  => 'mail',
+			'icon'        => 'mail',
 		);
 	}
 
@@ -46,7 +52,7 @@ function pmpro_dashboard_quick_links_callback() {
 			'href'        => add_query_arg( array( 'page' => 'pmpro-userfields' ), admin_url( 'admin.php' ) ),
 			'title'       => __( 'Manage User Fields', 'paid-memberships-pro' ),
 			'description' => __( 'Add or edit custom profile fields for users and members.', 'paid-memberships-pro' ),
-			'icon'  => 'edit-3',
+			'icon'        => 'edit-3',
 		);
 	}
 
@@ -55,14 +61,20 @@ function pmpro_dashboard_quick_links_callback() {
 			'href'        => add_query_arg( array( 'page' => 'pmpro-securitysettings' ), admin_url( 'admin.php' ) ),
 			'title'       => __( 'Manage Site Security', 'paid-memberships-pro' ),
 			'description' => __( 'Configure security settings for your membership site.', 'paid-memberships-pro' ),
-			'icon'  => 'shield',
+			'icon'        => 'shield',
 		);
 	}
 
 	// Show a link to view the filtered list of Add Ons they can install.
 	if ( current_user_can( 'pmpro_addons' ) ) {
-		$key = get_option( 'pmpro_license_key', '' );
-		$pmpro_license_check = get_option( 'pmpro_license_check', array( 'license' => false, 'enddate' => 0 ) );
+		$key                 = get_option( 'pmpro_license_key', '' );
+		$pmpro_license_check = get_option(
+			'pmpro_license_check',
+			array(
+				'license' => false,
+				'enddate' => 0,
+			)
+		);
 		if ( ! pmpro_license_isValid() && empty( $key ) ) {
 			$add_ons_type = 'free';
 		} elseif ( pmpro_license_isValid() && ! pmpro_license_isValid( $key, pmpro_license_get_premium_types() ) ) {
@@ -74,7 +86,7 @@ function pmpro_dashboard_quick_links_callback() {
 			'href'        => add_query_arg( array( 'page' => 'pmpro-addons#' . $add_ons_type ), admin_url( 'admin.php' ) ),
 			'title'       => __( 'View Add Ons', 'paid-memberships-pro' ),
 			'description' => sprintf( __( 'Explore available %s Add Ons for your membership site.', 'paid-memberships-pro' ), esc_html( $add_ons_type ) ),
-			'icon'  => 'download',
+			'icon'        => 'download',
 		);
 	}
 
