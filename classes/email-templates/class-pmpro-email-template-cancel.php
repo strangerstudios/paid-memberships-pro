f26f68766f57d43935c4e655cba653c033319b72
Kim Coleman <kim@strangerstudios.com>
2025-07-22 12:05:42 -0400

    Adding !!renew_url!! to appropriate email templates

--- classes/email-templates/class-pmpro-email-template-cancel.php
+++ classes/email-templates/class-pmpro-email-template-cancel.php
@@ -106,6 +106,7 @@ class PMPro_Email_Template_Cancel extends PMPro_Email_Template {
 			'!!user_email!!' => esc_html__( 'The email address of the user.', 'paid-memberships-pro' ),
 			'!!membership_id!!' => esc_html__( 'The ID of the membership level.', 'paid-memberships-pro' ),
 			'!!membership_level_name!!' => esc_html__( 'The name of the membership level.', 'paid-memberships-pro' ),
+			'!!renew_url!!' => esc_html__( 'The URL of the Membership Checkout page for the cancelled level.', 'paid-memberships-pro' ),
 		);
 	}
 
@@ -151,12 +152,15 @@ class PMPro_Email_Template_Cancel extends PMPro_Email_Template {
 		if ( empty( $this->cancelled_level_ids ) ) {
 			$email_template_variables['membership_id'] = '';
 			$email_template_variables['membership_level_name'] = esc_html__( 'All Levels', 'paid-memberships-pro' );
+			$email_template_variables['renew_url'] = pmpro_url( 'levels' );
 		} elseif ( is_array( $this->cancelled_level_ids ) ) {
 			$email_template_variables['membership_id'] = $this->cancelled_level_ids[0]; // Pass just the first as the level id.
 			$email_template_variables['membership_level_name'] = pmpro_implodeToEnglish( $wpdb->get_col( "SELECT name FROM $wpdb->pmpro_membership_levels WHERE id IN('" . implode( "','", $this->cancelled_level_ids ) . "')" ) );
+			$email_template_variables['renew_url'] = pmpro_url( 'levels' );
 		} else {
 			$email_template_variables['membership_id'] = $this->cancelled_level_ids;
 			$email_template_variables['membership_level_name'] = pmpro_implodeToEnglish( $wpdb->get_col( "SELECT name FROM $wpdb->pmpro_membership_levels WHERE id = '" . $this->cancelled_level_ids . "'" ) );
+			$email_template_variables['renew_url'] = pmpro_url( 'checkout', '?pmpro_level=' . $this->cancelled_level_ids );
 		}
 
 		return $email_template_variables;
