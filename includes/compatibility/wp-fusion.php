d194c04501582e941893798f5832106f0a1cb81d
Jarryd Long <JarrydLong@users.noreply.github.com>
2025-08-08 15:09:59 +0200

    Changed how we load the compat code

--- includes/compatibility/wp-fusion.php
+++ includes/compatibility/wp-fusion.php
@@ -1,37 +1,32 @@
 <?php
 /**
  * WP Fusion Compatibility
+ * This file runs in the plugins_loaded hook to ensure WP Fusion is loaded.
  * 
  * @since TBD
  */
 
-// Running this after plugins_loaded to ensure WP Fusion is loaded.
-function pmpro_wpfusion_compatibility() {
-
-    /**
-     * WP Fusion Lite is not active, we shouldn't run anything WP Fusion related for now.
-     */
-    if( ! class_exists( 'WP_Fusion_Lite' ) ) {
-        return;
-    }
-
-    /**
-     * WP Fusion (which has PMPro in it is running), let their code handle things
-     */
-    if( class_exists( 'WP_Fusion' ) ) {
-        return;
-    }
-
-    /**
-     * ToDo
-     * Rename these classes to prevent any potential conflicts with WP Fusion.
-     */
-    include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-base.php' );
-    include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro.php' );
-    include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-hooks.php' );
-    include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-batch.php' );
-    include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-approvals.php' );
-    include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-admin.php' );
-
+/**
+ * WP Fusion Lite is not active, we shouldn't run anything WP Fusion related for now.
+ */
+if( ! class_exists( 'WP_Fusion_Lite' ) ) {
+    return;
 }
-add_action( 'plugins_loaded', 'pmpro_wpfusion_compatibility' );
+
+/**
+ * WP Fusion (which has PMPro in it is running), let their code handle things
+ */
+if( class_exists( 'WP_Fusion' ) ) {
+    return;
+}
+
+/**
+ * ToDo
+ * Rename these classes to prevent any potential conflicts with WP Fusion.
+ */
+include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-base.php' );
+include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro.php' );
+include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-hooks.php' );
+include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-batch.php' );
+include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-approvals.php' );
+include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-admin.php' );
