16e1bbd57a59c402a7479250f2dc7d29a3d23f8b
Andrew Lima <andrew@arctek.co.za>
2025-08-08 13:53:38 +0200

    Minor refactor for comments.

--- includes/compatibility/wp-fusion.php
+++ includes/compatibility/wp-fusion.php
@@ -1,37 +1,33 @@
 <?php
 /**
- * WP Fusion Compatibility
+ * WP Fusion Compatibility for Paid Memberships Pro.
  * 
  * @since TBD
  */
 
-// Running this after plugins_loaded to ensure WP Fusion is loaded.
+/**
+ * Logic to enable PMPro WP Fusion (Lite) compatibility when WP Fusion Lite is active.
+ * 
+ * @since TBD
+ */
 function pmpro_wpfusion_compatibility() {
 
-    /**
-     * WP Fusion Lite is not active, we shouldn't run anything WP Fusion related for now.
-     */
-    if( ! class_exists( 'WP_Fusion_Lite' ) ) {
+    // If WP Fusion Lite is not active, then we don't need to do anything.
+    if ( ! class_exists( 'WP_Fusion_Lite' ) ) {
         return;
     }
 
-    /**
-     * WP Fusion (which has PMPro in it is running), let their code handle things
-     */
-    if( class_exists( 'WP_Fusion' ) ) {
+    // If WP Fusion (which has PMPro in it) is running, let their code handle things
+    if ( class_exists( 'WP_Fusion' ) ) {
         return;
     }
 
-    /**
-     * ToDo
-     * Rename these classes to prevent any potential conflicts with WP Fusion.
-     */
+    // Include the WP Fusion PMPro specific classes.
     include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-base.php' );
     include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro.php' );
     include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-hooks.php' );
     include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-batch.php' );
     include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-approvals.php' );
     include_once( PMPRO_DIR . '/includes/compatibility/wp-fusion/class-pmpro-admin.php' );
-
 }
 add_action( 'plugins_loaded', 'pmpro_wpfusion_compatibility' );
