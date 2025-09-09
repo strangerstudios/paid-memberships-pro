9ed2a82200f56412332452449b6971ea67da37f3
Dale Mugford <dmugford@paidmembershipspro.com>
2025-09-09 15:39:17 -0400

    dropdown wiring to actions, state management
    Signed-off-by: Dale Mugford <dmugford@paidmembershipspro.com>

--- js/pmpro-admin.js
+++ js/pmpro-admin.js
@@ -780,20 +780,21 @@ jQuery(document).ready(function () {
 		}
 	});
 
-	jQuery('#pmpro-admin-add-ons-list .action-button .pmproAddOnActionButton').click(function (e) {
+	jQuery('#pmpro-admin-add-ons-list').on('click', '.pmproAddOnActionButton', function (e) {
 		e.preventDefault();
 
 		var button = jQuery(this);
+		var container = button.closest('.add-on-container');
+		var isMainButton = button.parent().hasClass('action-button');
 
 		// Make sure we only run once.
-		button.hasClass('disabled')
 		if (button.hasClass('disabled')) {
 			return;
 		}
 		button.addClass('disabled');
 
 		// Pull the action that we are performing on this button.
-	var action = button.siblings('input[name="pmproAddOnAdminAction"]').val();
+		var action = button.siblings('input[name="pmproAddOnAdminAction"]').val();
 
 		if ('license' === action) {
 			// Get the add on name and the user's current license type and show banner.
@@ -802,100 +803,267 @@ jQuery(document).ready(function () {
 			jQuery('.pmpro-popup-overlay').show();
 			button.removeClass('disabled');
 			return false;
-		} else {
-			// Remove checkmark if there.
-			button.removeClass('checkmarked');
+		}
 
-			// Update button text
-			if ('activate' === action) {
-				button.text('Activating...');
-			} else if ('install' === action) {
-				button.text('Installing...');
-			} else if ('update' === action) {
-				button.text('Updating...');
-			} else if ('deactivate' === action) {
-				button.text('Deactivating...');
-			} else if ('delete' === action) {
-				button.text('Deleting...');
-			} else {
+		// Remove checkmark if there.
+		button.removeClass('checkmarked');
+
+		// Update button text (progress states)
+		var progressMap = {
+			'activate': 'Activating...',
+			'install': 'Installing...',
+			'update': 'Updating...',
+			'deactivate': 'Deactivating...',
+			'delete': 'Deleting...'
+		};
+		if (!progressMap[action]) {
+			button.removeClass('disabled');
+			return;
+		}
+		button.text(progressMap[action]);
+
+		// Build AJAX payload for new class endpoints
+		var target = button.siblings('input[name="pmproAddOnAdminTarget"]').val();
+		var nonce = button.siblings('input[name="pmproAddOnAdminNonce"]').val();
+		var network_wide = button.siblings('input[name="pmproAddOnNetworkWide"]').val() === '1';
+		var ajaxAction = null;
+		if (action === 'install') ajaxAction = 'pmpro_addon_install';
+		if (action === 'update') ajaxAction = 'pmpro_addon_update';
+		if (action === 'activate') ajaxAction = 'pmpro_addon_activate';
+		if (action === 'deactivate') ajaxAction = 'pmpro_addon_deactivate';
+		if (action === 'delete') ajaxAction = 'pmpro_addon_delete';
+
+		function recountFilters(){
+			var activeCount = jQuery('.add-on-container.add-on-active').length;
+			var inactiveCount = jQuery('.add-on-container.add-on-inactive').length;
+			var updateCount = jQuery('.add-on-container.add-on-needs-update').length;
+			function setCount($link, count, hide){
+				if(!$link.length){return;}
+				var baseLabel = $link.data('baseLabel');
+				if(!baseLabel){
+					baseLabel = jQuery.trim($link.text()).replace(/\s*\(\d+\)$/,'');
+					$link.data('baseLabel', baseLabel);
+				}
+				$link.text(count>0? baseLabel+' ('+count+')': baseLabel);
+				if(hide){ $link.closest('li').toggle(count>0); }
+			}
+			setCount(jQuery('.filter-links a[data-view="active"]'), activeCount, false);
+			setCount(jQuery('.filter-links a[data-view="inactive"]'), inactiveCount, false);
+			setCount(jQuery('.filter-links a[data-view="update"]'), updateCount, true);
+			if(updateCount===0){
+				var $updateLink = jQuery('.filter-links a[data-view="update"]');
+				if($updateLink.hasClass('current') || window.location.hash === '#update'){
+					jQuery('.filter-links a[data-view="all"]').trigger('click');
+				}
+			}
+		}
+
+		function applyCurrentFilter(){
+			var $current = jQuery('.filter-links a.current');
+			if($current.length){ $current.trigger('click'); }
+		}
+
+		jQuery.post(ajaxurl, {
+			action: ajaxAction,
+			nonce: nonce,
+			target: target,
+			slug: (action === 'install' ? target : ''),
+			network_wide: network_wide ? 1 : 0
+		}).done(function(resp){
+			if (!resp || !resp.success) {
+				var msg = (resp && resp.data && resp.data.message) ? resp.data.message : 'Action failed.';
+				// Filesystem credentials needed
+				if (resp && resp.data && resp.data.code === 'pmpro_fs_credentials') {
+					button.text('Credentials required...');
+					if (window.wp && wp.updates && wp.updates.maybeRequestFilesystemCredentials) {
+						wp.updates.maybeRequestFilesystemCredentials({});
+					}
+				} else {
+					button.text(msg);
+				}
+				button.removeClass('disabled');
 				return;
 			}
 
-			// Build AJAX payload for new class endpoints
-			var target = button.siblings('input[name="pmproAddOnAdminTarget"]').val();
-			var nonce = button.siblings('input[name="pmproAddOnAdminNonce"]').val();
-			var network_wide = button.siblings('input[name="pmproAddOnNetworkWide"]').val() === '1';
-			var ajaxAction = null;
-			if (action === 'install') ajaxAction = 'pmpro_addon_install';
-			if (action === 'update') ajaxAction = 'pmpro_addon_update';
-			if (action === 'activate') ajaxAction = 'pmpro_addon_activate';
-			if (action === 'deactivate') ajaxAction = 'pmpro_addon_deactivate';
-			if (action === 'delete') ajaxAction = 'pmpro_addon_delete';
+			var plugin_file = (resp && resp.data && resp.data.plugin_file) ? resp.data.plugin_file : null;
+			var slug = container.attr('id');
+			if(!plugin_file && slug){ plugin_file = slug + '/' + slug + '.php'; }
+			var mainButton = container.find('.action-button > .pmproAddOnActionButton');
+			var mainActionInput = mainButton.siblings('input[name="pmproAddOnAdminAction"]');
+			var mainTargetInput = mainButton.siblings('input[name="pmproAddOnAdminTarget"]');
+			var statusLabel = container.find('.add-on-status strong');
+			var dropdownBtn = container.find('.details > .dropdown-arrow');
+			var dropdownMenu = container.find('.pmpro-add-on-actions-menu');
+			var firstMenuBtn = dropdownMenu.find('button.pmproAddOnActionButton').first();
+			var firstMenuActionInput = firstMenuBtn.siblings('input[name="pmproAddOnAdminAction"]');
 
-			jQuery.post(ajaxurl, {
-				action: ajaxAction,
-				nonce: nonce,
-				target: target,
-				slug: (action === 'install' ? target : ''),
-				network_wide: network_wide ? 1 : 0
-			}).done(function(resp){
-				if (!resp || !resp.success) {
-					var msg = (resp && resp.data && resp.data.message) ? resp.data.message : 'Action failed.';
-					// Filesystem credentials needed
-					if (resp && resp.data && resp.data.code === 'pmpro_fs_credentials') {
-						button.text('Credentials required...');
-						if (window.wp && wp.updates && wp.updates.maybeRequestFilesystemCredentials) {
-							wp.updates.maybeRequestFilesystemCredentials({});
-						}
-					} else {
-						button.text(msg);
-					}
-					button.removeClass('disabled');
-					return;
+			function setStatus(newStatus, statusText){
+				container.removeClass('add-on-active add-on-inactive add-on-uninstalled');
+				if(newStatus){ container.addClass('add-on-' + newStatus); }
+				if(statusLabel.length){
+					statusLabel.removeClass(function(i,c){ return (c.match(/status-\S+/g)||[]).join(' '); });
+					statusLabel.attr('class','status-' + newStatus).text(statusText);
 				}
+				// Update data-search-view tokens
+				var viewAttr = container.data('search-view');
+				if(viewAttr){
+					var parts = viewAttr.split(' ').filter(function(p){ return ['active','inactive','update'].indexOf(p) === -1; });
+					if(newStatus === 'active') parts.push('active');
+					if(newStatus === 'inactive') parts.push('inactive');
+					if(container.hasClass('add-on-needs-update')) parts.push('update');
+					var newView = parts.filter(function(v,i,a){ return a.indexOf(v)===i; }).join(' ');
+					container.attr('data-search-view', newView);
+					container.data('search-view', newView);
+				}
+			}
 
-				// Success UI
-				button.addClass('checkmarked');
-				if (action === 'activate') {
-					button.text('Activated');
-				} else if (action === 'install') {
-					button.text('Installed');
-					// Switch to Activate state
+			// Handle each action result
+			if (action === 'activate') {
+				button.text('Activated').addClass('checkmarked');
+				// Main button shows Active (disabled)
+				if(mainButton.length){
+					mainButton.text('Active').addClass('disabled').attr('disabled','disabled').attr('aria-disabled','true');
+					mainActionInput.val('');
+					mainTargetInput.val(plugin_file);
+				}
+				// Update dropdown first item to Deactivate
+				if(firstMenuBtn.length){
+					firstMenuBtn.text('Deactivate').removeClass('action-activate').addClass('action-deactivate');
+					firstMenuActionInput.val('deactivate');
+				}
+				setStatus('active', 'Active');
+			} else if (action === 'deactivate') {
+				button.text('Deactivated').addClass('checkmarked');
+				if(mainButton.length){
+					mainButton.text('Activate').removeClass('disabled').removeAttr('disabled').attr('aria-disabled','false');
+					mainActionInput.val('activate');
+					mainTargetInput.val(plugin_file);
+				}
+				if(firstMenuBtn.length){
+					firstMenuBtn.text('Activate').removeClass('action-deactivate').addClass('action-activate');
+					firstMenuActionInput.val('activate');
+				}
+				setStatus('inactive', 'Inactive');
+			} else if (action === 'install') {
+				button.text('Installed').addClass('checkmarked');
+				// After install the plugin is inactive.
+				if(mainButton.length){
 					setTimeout(function(){
-						button.siblings('input[name="pmproAddOnAdminAction"]').val('activate');
-						button.text('Activate');
-						button.removeClass('disabled checkmarked');
-					}, 800);
-					return;
-				} else if (action === 'update') {
-					button.text('Updated');
-				} else if (action === 'deactivate') {
-					button.text('Deactivated');
-				} else if (action === 'delete') {
-					button.text('Deleted');
+						mainButton.text('Activate').removeClass('disabled checkmarked');
+						mainActionInput.val('activate');
+						mainTargetInput.val(plugin_file);
+					}, 600);
 				}
+				setStatus('inactive', 'Inactive');
+			} else if (action === 'update') {
+				button.text('Updated').addClass('checkmarked');
+				container.removeClass('add-on-needs-update');
+				// If active keep Active label, else ensure main button is Activate
+				if(mainButton.length){
+					if(container.hasClass('add-on-active')){
+						mainButton.text('Active').addClass('disabled').attr('disabled','disabled').attr('aria-disabled','true');
+					}else{
+						mainButton.text('Activate').removeClass('disabled');
+						mainActionInput.val('activate');
+						mainTargetInput.val(plugin_file);
+					}
+				}
+				// Update search view attribute removing update token
+				var viewAttr2 = container.data('search-view');
+				if(viewAttr2){
+					var parts2 = viewAttr2.split(' ').filter(function(p){ return p !== 'update'; });
+					container.attr('data-search-view', parts2.join(' '));
+					container.data('search-view', parts2.join(' '));
+				}
+			} else if (action === 'delete') {
+				button.text('Deleted').addClass('checkmarked');
+				// Switch to uninstalled state
+				setStatus('uninstalled', 'Not Installed');
+				if(mainButton.length){
+					setTimeout(function(){
+						mainButton.text('Install').removeClass('disabled checkmarked');
+						mainActionInput.val('install');
+						mainTargetInput.val(slug); // target is slug for install
+					}, 600);
+				}
+				// Remove dropdown
+				dropdownBtn.remove();
+				dropdownMenu.remove();
+			}
 
-				// Re-enable after success for possible further actions
-				setTimeout(function(){
-					button.removeClass('disabled');
-				}, 400);
-			}).fail(function(){
-				if (action === 'activate') {
-					button.text('Could Not Activate.');
-				} else if (action === 'install') {
-					button.text('Could Not Install.');
-				} else if (action === 'update') {
-					button.text('Could Not Update.');
-				} else if (action === 'deactivate') {
-					button.text('Could Not Deactivate.');
-				} else if (action === 'delete') {
-					button.text('Could Not Delete.');
-				}
+			recountFilters();
+			applyCurrentFilter();
+
+			// Re-enable after success for possible further actions (except transient states already swapped)
+			setTimeout(function(){
 				button.removeClass('disabled');
-			});
+			}, 400);
+		}).fail(function(){
+			var failMap = {
+				'activate': 'Could Not Activate.',
+				'install': 'Could Not Install.',
+				'update': 'Could Not Update.',
+				'deactivate': 'Could Not Deactivate.',
+				'delete': 'Could Not Delete.'
+			};
+			button.text(failMap[action] || 'Action failed.');
+			button.removeClass('disabled');
+		});
+	});
+});
 
+// Add On Action Dropdown (toggle + accessibility)
+jQuery(document).ready(function(){
+	var $doc = jQuery(document);
+
+	function closeAllAddonMenus(except){
+		jQuery('#pmpro-admin-add-ons-list .add-on-item .dropdown-arrow').attr('aria-expanded','false');
+		jQuery('#pmpro-admin-add-ons-list .add-on-item .pmpro-add-on-actions-menu').attr('aria-hidden','true');
+		jQuery('#pmpro-admin-add-ons-list .add-on-item').removeClass('is-open');
+		if (except) {
+			except.attr('aria-expanded','true')
+				.closest('.add-on-item').addClass('is-open')
+				.find('.pmpro-add-on-actions-menu').attr('aria-hidden','false');
+		}
+	}
+
+	$doc.on('click', '#pmpro-admin-add-ons-list .add-on-item .dropdown-arrow', function(e){
+		e.preventDefault();
+		var $btn = jQuery(this);
+		var isOpen = $btn.attr('aria-expanded') === 'true';
+		if (isOpen) {
+			closeAllAddonMenus();
+		} else {
+			closeAllAddonMenus($btn);
+			// Focus first actionable item for keyboard users
+			setTimeout(function(){
+				var $first = $btn.next('.pmpro-add-on-actions-menu').find('button:not([disabled])').first();
+				if ($first.length) { $first.trigger('focus'); }
+			}, 15);
 		}
 	});
+
+	// Keyboard navigation inside menu
+	$doc.on('keydown', '#pmpro-admin-add-ons-list .pmpro-add-on-actions-menu', function(e){
+		var $items = jQuery(this).find('button:not([disabled])');
+		if (!$items.length) { return; }
+		var idx = $items.index(document.activeElement);
+		if (e.key === 'ArrowDown') { e.preventDefault(); idx = (idx + 1) % $items.length; $items.eq(idx).focus(); }
+		else if (e.key === 'ArrowUp') { e.preventDefault(); idx = (idx - 1 + $items.length) % $items.length; $items.eq(idx).focus(); }
+		else if (e.key === 'Home') { e.preventDefault(); $items.eq(0).focus(); }
+		else if (e.key === 'End') { e.preventDefault(); $items.eq($items.length - 1).focus(); }
+		else if (e.key === 'Escape') { e.preventDefault(); closeAllAddonMenus(); }
+	});
+
+	// Close when clicking outside
+	$doc.on('click', function(e){
+		if (!jQuery(e.target).closest('#pmpro-admin-add-ons-list .add-on-item .pmpro-add-on-actions-menu, #pmpro-admin-add-ons-list .add-on-item .dropdown-arrow').length) {
+			closeAllAddonMenus();
+		}
+	});
+
+	// Close on Escape from anywhere
+	$doc.on('keyup', function(e){ if (e.key === 'Escape') { closeAllAddonMenus(); } });
 });
 
 /**
