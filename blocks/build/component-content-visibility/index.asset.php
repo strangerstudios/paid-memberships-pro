5906a0a66a1e406676586c759f9f8481a4dcd7f6
David Parker <dlparker1005@gmail.com>
2026-02-05 16:02:14 -0500

    Building blocks

--- blocks/build/component-content-visibility/index.asset.php
+++ blocks/build/component-content-visibility/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b2473602959c6dc1170a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8ae5d3d8b21a2f78bf94');
--- blocks/build/sidebar/index.asset.php
+++ blocks/build/sidebar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-element'), 'version' => 'f47988768109e49d5c9c');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-element'), 'version' => '7d4406cc74ae94141b8f');
